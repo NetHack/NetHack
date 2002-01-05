9a99748a301e9a96c11df3f9a047b6913450ceb8
jwalz <jwalz>
2002-01-05 21:06:01 +0000

    *** empty log message ***

---
+++ win/gnome/gnmenu.h
@@ -0,0 +1,32 @@
+/*	SCCS Id: @(#)gnmenu.h	3.3	2000/07/16	*/
+/* Copyright (C) 1998 by Erik Andersen <andersee@debian.org> */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#ifndef GnomeHackMenuWindow_h
+#define GnomeHackMenuWindow_h
+
+#include <gnome.h>
+#include "config.h"
+#include "global.h"
+#include "gnomeprv.h"
+
+GtkWidget* ghack_init_menu_window( void );
+
+struct _GHackMenuItem
+{
+  int		glyph; 
+  const ANY_P *identifier;
+  CHAR_P       accelerator;
+  CHAR_P       group_accel;
+  int          attr;
+  const char*  str;
+  BOOLEAN_P    presel;
+};
+
+typedef struct _GHackMenuItem GHackMenuItem;
+
+int ghack_menu_window_select_menu (GtkWidget *menuWin, 
+	MENU_ITEM_P **_selected, gint how);
+
+
+#endif  /* GnomeHackMenuWindow_h */
