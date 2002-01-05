48205584092e0235f1255bb57eb5fc440fce97d7
jwalz <jwalz>
2002-01-05 21:05:48 +0000

    *** empty log message ***

---
+++ include/qt_kde0.h
@@ -0,0 +1,10 @@
+/*	SCCS Id: @(#)qt_kde0.h	3.3	1999/11/19	*/
+/* Copyright (c) Warwick Allison, 1999. */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#ifndef QT_DUMMYKDE
+#define QT_DUMMYKDE
+class KTopLevelWidget : public QMainWindow {
+        Q_OBJECT
+};
+#endif
