15bc7496e462a51065d8301fd40dcfda3d532aac
jwalz <jwalz>
2002-01-05 21:06:00 +0000

    *** empty log message ***

---
+++ sys/winnt/ntsound.c
@@ -0,0 +1,15 @@
+/*   SCCS Id: @(#)ntsound.c   3.3     95/09/06                        */
+/*   Copyright (c) NetHack PC Development Team 1993                 */
+/*   NetHack may be freely redistributed.  See license for details. */
+/*                                                                  */
+/*
+ * ntsound.c - Windows NT NetHack sound support
+ *                                                  
+ *Edit History:
+ *     Initial Creation                              93/12/11
+ *
+ */
+
+#include "hack.h"
+
+/* ntsound.c */
