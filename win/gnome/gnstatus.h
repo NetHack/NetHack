a87456756a7ed4dcdf900a8f7f945e7fabea6d10
jwalz <jwalz>
2002-01-05 21:06:01 +0000

    *** empty log message ***

---
+++ win/gnome/gnstatus.h
@@ -0,0 +1,14 @@
+/*	SCCS Id: @(#)gnstatus.h	3.3	2000/07/16	*/
+/* Copyright (C) 1998 by Erik Andersen <andersee@debian.org> */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#ifndef GnomeHackStatusWindow_h
+#define GnomeHackStatusWindow_h
+
+#include <gnome.h>
+#include "config.h"
+#include "global.h"
+
+GtkWidget* ghack_init_status_window ();
+
+#endif /* GnomeHackStatusWindow_h */
