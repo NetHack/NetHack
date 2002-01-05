cc7dd25c978547a3d9d003b6d431b7be27353517
jwalz <jwalz>
2002-01-05 21:06:01 +0000

    *** empty log message ***

---
+++ win/gnome/gnopts.h
@@ -0,0 +1,13 @@
+/*	SCCS Id: @(#)gnopts.h	3.3	2000/07/16	*/
+/* Copyright (C) 1998 by Erik Andersen <andersee@debian.org> */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#ifndef GnomeHackSettings_h
+#define GnomeHackSettings_h
+
+
+void ghack_settings_dialog( void);
+
+
+#endif /* GnomeHackSettings.h */
+
