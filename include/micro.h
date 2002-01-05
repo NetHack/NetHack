cdf98cd65b1d7ee8f0542041b3c019844815d276
jwalz <jwalz>
2002-01-05 21:05:47 +0000

    *** empty log message ***

---
+++ include/micro.h
@@ -0,0 +1,21 @@
+/*	SCCS Id: @(#)micro.h	3.3	90/02/22	*/
+/* micro.h - function declarations for various microcomputers */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#ifndef MICRO_H
+#define MICRO_H
+
+extern const char *alllevels, *allbones;
+extern char levels[], bones[], permbones[], hackdir[];
+
+extern int ramdisk;
+
+#ifndef C
+#define C(c)	(0x1f & (c))
+#endif
+#ifndef M
+#define M(c)	(0x80 | (c))
+#endif
+#define ABORT C('a')
+
+#endif /* MICRO_H */
