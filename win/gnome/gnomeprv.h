36366da6f449de99931d27bc8e08c3bb89eaef35
jwalz <jwalz>
2002-01-05 21:06:01 +0000

    *** empty log message ***

---
+++ win/gnome/gnomeprv.h
@@ -0,0 +1,16 @@
+/*	SCCS Id: @(#)gnomeprv.h	3.3	2000/07/16	*/
+/* Copyright (C) 1998 by Erik Andersen <andersee@debian.org> */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#ifndef GnomeHack_h
+#define GnomeHack_h
+
+/* These are the base nethack include files */
+#include "hack.h"
+#include "dlb.h"
+#include "patchlevel.h"
+
+#endif    /* GnomeHack_h */
+
+
+
