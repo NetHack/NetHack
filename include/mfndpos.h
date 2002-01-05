c04563fb18ba8b90d8468917fdc58c211ad6af6b
jwalz <jwalz>
2002-01-05 21:05:47 +0000

    *** empty log message ***

---
+++ include/mfndpos.h
@@ -0,0 +1,28 @@
+/*	SCCS Id: @(#)mfndpos.h	3.3	93/05/15	*/
+/* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#ifndef MFNDPOS_H
+#define MFNDPOS_H
+
+#define ALLOW_TRAPS	0x00020000L	/* can enter traps */
+#define ALLOW_U		0x00040000L	/* can attack you */
+#define ALLOW_M		0x00080000L	/* can attack other monsters */
+#define ALLOW_TM	0x00100000L	/* can attack tame monsters */
+#define ALLOW_ALL	(ALLOW_U | ALLOW_M | ALLOW_TM | ALLOW_TRAPS)
+#define NOTONL		0x00200000L	/* avoids direct line to player */
+#define OPENDOOR	0x00400000L	/* opens closed doors */
+#define UNLOCKDOOR	0x00800000L	/* unlocks locked doors */
+#define BUSTDOOR	0x01000000L	/* breaks any doors */
+#define ALLOW_ROCK	0x02000000L	/* pushes rocks */
+#define ALLOW_WALL	0x04000000L	/* walks thru walls */
+#define ALLOW_DIG	0x08000000L	/* digs */
+#define ALLOW_SANCT	0x20000000L	/* enters temples */
+#define ALLOW_SSM	0x40000000L	/* ignores scare monster */
+#ifdef NHSTDC
+#define NOGARLIC	0x80000000UL	/* hates garlic */
+#else
+#define NOGARLIC	0x80000000L	/* hates garlic */
+#endif
+
+#endif /* MFNDPOS_H */
