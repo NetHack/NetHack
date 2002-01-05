4bb7691d1d9d9ddfdd2bbca819a0643721152679
jwalz <jwalz>
2002-01-05 21:05:47 +0000

    *** empty log message ***

---
+++ include/emin.h
@@ -0,0 +1,14 @@
+/*	SCCS Id: @(#)emin.h	3.3	97/05/01	*/
+/* Copyright (c) David Cohrs, 1990.				  */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#ifndef EMIN_H
+#define EMIN_H
+
+struct emin {
+	aligntyp min_align;	/* alignment of minion */
+};
+
+#define EMIN(mon)	((struct emin *)&(mon)->mextra[0])
+
+#endif /* EMIN_H */
