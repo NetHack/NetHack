a8d7c5c10338c2cbc71ee5bfe5193cc9de7c83e8
jwalz <jwalz>
2002-01-05 21:05:48 +0000

    *** empty log message ***

---
+++ include/rect.h
@@ -0,0 +1,13 @@
+/*	SCCS Id: @(#)rect.h	3.3	90/02/22	*/
+/* Copyright (c) 1990 by Jean-Christophe Collet			  */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#ifndef RECT_H
+#define RECT_H
+
+typedef struct nhrect {
+	xchar lx, ly;
+	xchar hx, hy;
+} NhRect;
+
+#endif /* RECT_H */
