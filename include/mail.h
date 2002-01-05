1af904fae0d7d7cca24632853423e4ee7050caff
jwalz <jwalz>
2002-01-05 21:05:47 +0000

    *** empty log message ***

---
+++ include/mail.h
@@ -0,0 +1,20 @@
+/*	SCCS Id: @(#)mail.h	3.3	91/10/11	*/
+/* NetHack may be freely redistributed.  See license for details. */
+
+/* used by ckmailstatus() to pass information to the mail-daemon in newmail() */
+
+#ifndef MAIL_H
+#define MAIL_H
+
+#define MSG_OTHER 0	/* catch-all; none of the below... */
+#define MSG_MAIL  1	/* unimportant, uninteresting mail message */
+#define MSG_CALL  2	/* annoying phone/talk/chat-type interruption */
+
+struct mail_info {
+	int	 message_typ;		/* MSG_foo value */
+	const char *display_txt;	/* text for daemon to verbalize */
+	const char *object_nam;		/* text to tag object with */
+	const char *response_cmd;	/* command to eventually execute */
+};
+
+#endif /* MAIL_H */
