e283de0a5aa0beea8e1a1d29205368c4a10a9aa6
jwalz <jwalz>
2002-01-05 21:05:47 +0000

    *** empty log message ***

---
+++ include/epri.h
@@ -0,0 +1,24 @@
+/*	SCCS Id: @(#)epri.h	3.3	97/05/01	*/
+/* Copyright (c) Izchak Miller, 1989.				  */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#ifndef EPRI_H
+#define EPRI_H
+
+struct epri {
+	aligntyp shralign;	/* alignment of priest's shrine */
+				/* leave as first field to match emin */
+	schar shroom;		/* index in rooms */
+	coord shrpos;		/* position of shrine */
+	d_level shrlevel;	/* level (& dungeon) of shrine */
+};
+
+#define EPRI(mon)	((struct epri *)&(mon)->mextra[0])
+
+/* A priest without ispriest is a roaming priest without a shrine, so
+ * the fields (except shralign, which becomes only the priest alignment)
+ * are available for reuse.
+ */
+#define renegade shroom
+
+#endif /* EPRI_H */
