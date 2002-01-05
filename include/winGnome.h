0dd578adf4bd6cba364eb24ace6313e8da10be0e
jwalz <jwalz>
2002-01-05 21:05:48 +0000

    *** empty log message ***

---
+++ include/winGnome.h
@@ -0,0 +1,15 @@
+/*	SCCS Id: @(#)winGnome.h	3.3.	2000/07/16	*/
+/* Copyright (C) 1998 by Erik Andersen <andersee@debian.org> */
+/* Copyright (C) 1998 by Anthony Taylor <tonyt@ptialaska.net> */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#ifndef WINGNOME_H
+#define WINGNOME_H
+
+#define E extern
+
+E struct window_procs Gnome_procs;
+
+#undef E
+
+#endif /* WINGNOME_H */
