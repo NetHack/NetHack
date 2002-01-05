b85877fdd875ae87887d58a8c31570e0d3b501d1
jwalz <jwalz>
2002-01-05 21:05:47 +0000

    *** empty log message ***

---
+++ include/coord.h
@@ -0,0 +1,12 @@
+/*	SCCS Id: @(#)coord.h	3.3	90/02/22	*/
+/* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#ifndef COORD_H
+#define COORD_H
+
+typedef struct nhcoord {
+	xchar x,y;
+} coord;
+
+#endif /* COORD_H */
