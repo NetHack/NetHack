3a794198b96747492e08091c4d0513207ed01d38
jwalz <jwalz>
2002-01-05 21:06:01 +0000

    *** empty log message ***

---
+++ win/gnome/gnmap.h
@@ -0,0 +1,16 @@
+/*	SCCS Id: @(#)gnmap.h	3.3	2000/07/16	*/
+/* Copyright (C) 1998 by Erik Andersen <andersee@debian.org> */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#ifndef GnomeHackMapWindow_h
+#define GnomeHackMapWindow_h
+
+#include <gnome.h>
+#include <gdk_imlib.h>
+#include "config.h"
+#include "global.h"
+
+GtkWidget *ghack_init_map_window(void);
+void ghack_reinit_map_window(void);
+
+#endif /* GnomeHackMapWindow_h */
