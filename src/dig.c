c286e485702103876da00da9f14abb4092823a62
nethack.allison <nethack.allison>
2002-07-17 23:26:48 +0000

    remove sparks lighting oil
    for now at least, until there is more time to look into it.
    
    <email deleted>
    > Yet more nitpicking about potions of oil being lit by sparks from axed
    > statues, I'm afraid.
    >
    > Konosja's potions of oil catch light!
    > "That will cost you 66 zorkmids (Yendorian Fuel Tax)."
    > "That's in addition to the cost of Konosja's potions of oil themselves, of
    > course."
    > You snuff the lit potion.
    >
    > a) why is the Fuel Tax on (in this case) six potions the same as on one?
    >
    > b) if you a)pply-light a potion, it's just "in addition to the cost of
    >    the potion". Is Yname2() the right name-function for catch_lit() to
    >    be using? It's odd for Konosja to be talking about herself in the
    >    third person.
    >
    > c) Grammar on the snuffing message is wrong for multiple potions.

--- src/dig.c
+++ src/dig.c
@@ -912,13 +912,11 @@ struct obj *obj;
 			    You("need a pick to dig rock.");
 			else if (!ispick && (sobj_at(STATUE, rx, ry) ||
 					     sobj_at(BOULDER, rx, ry))) {
-			    struct obj *otmp;
 			    boolean vibrate = !rn2(3);
 			    pline("Sparks fly as you whack the %s.%s",
 				sobj_at(STATUE, rx, ry) ? "statue" : "boulder",
 				vibrate ? " The axe-handle vibrates violently!" : "");
 			    if (vibrate) losehp(2, "axing a hard object", KILLED_BY);
-			    if ((otmp = carrying(POT_OIL))) catch_lit(otmp);
 			}
 			else
 			    You("swing your %s through thin air.",
