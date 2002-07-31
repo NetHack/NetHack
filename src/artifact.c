0823cb41be9b7b9c9ed1817d716c9330401c36eb
cohrs <cohrs>
2002-07-31 16:47:49 +0000

    R1024 - Eyes of the Overworld effect delayed The effect of the EOTO did not take effect until the next move. Added a vision_full_recalc as suggested to resolve the problem.

--- src/artifact.c
+++ src/artifact.c
@@ -439,6 +439,7 @@ long wp_mask;
 	    /* this assumes that no one else is using xray_range */
 	    if (on) u.xray_range = 3;
 	    else u.xray_range = -1;
+	    vision_full_recalc = 1;
 	}
 	if ((spfx & SPFX_REFLECT) && (wp_mask & W_WEP)) {
 	    if (on) EReflecting |= wp_mask;
