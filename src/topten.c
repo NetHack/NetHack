252460c70a1a8224c7c13493b2f20ba79bf3e611
cohrs <cohrs>
2003-05-17 22:03:27 +0000

    starvation killer message Write "died of starvation" on the tombstone, not just "starvation". Suggested by <Someone> a couple weeks ago, although his suggested prefix was different and didn't work as well for the "exhaustion" case.

--- src/topten.c
+++ src/topten.c
@@ -77,7 +77,7 @@ STATIC_DCL void FDECL(nsb_unmung_line,(char*));
 
 /* must fit with end.c; used in rip.c */
 NEARDATA const char * const killed_by_prefix[] = {
-	"killed by ", "choked on ", "poisoned by ", "", "drowned in ",
+	"killed by ", "choked on ", "poisoned by ", "died of ", "drowned in ",
 	"burned by ", "dissolved in ", "crushed to death by ", "petrified by ",
 	"turned to slime by ", "killed by ", "", "", "", "", ""
 };
