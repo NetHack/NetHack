48d826b9079677c159eabb9cff01e686a9e6f2b2
kmhugo <kmhugo>
2003-09-02 16:06:53 +0000

    Mac Terminal port tuneup
    Submit the last of the changes that were needed to compile the
    MacOS X Terminal port.

--- include/mac-term.h
+++ include/mac-term.h
@@ -20,14 +20,15 @@
  *		set "Prefix File" to include/mac-term.h
  */
 
+/* Stuff needed for the core of NetHack */
 #undef MAC
 #define UNIX
 #define BSD
+#define __FreeBSD__ 	/* Darwin is based on FreeBSD */
+#define GCC_WARN
 
 /* May already be defined by CodeWarrior as 0 or 1 */
 #ifdef TARGET_API_MAC_CARBON
 # undef TARGET_API_MAC_CARBON
 #endif
-#define TARGET_API_MAC_CARBON 0
-
-#define GCC_WARN
+#define TARGET_API_MAC_CARBON 0 	/* Not Carbon */
