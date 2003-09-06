89551f2157598e5d71799a03dd19a85642bec05b
nethack.allison <nethack.allison>
2003-09-06 21:33:31 +0000

    missing band <Someone> pointed out that I didn't check in the updated include/extern.h

--- include/extern.h
+++ include/extern.h
@@ -938,7 +938,7 @@ E void FDECL(mimic_hit_msg, (struct monst *, SHORT_P));
 E void FDECL(mkmonmoney, (struct monst *, long));
 #endif
 E void FDECL(bagotricks, (struct obj *));
-E boolean FDECL(propagate, (int, BOOLEAN_P));
+E boolean FDECL(propagate, (int, BOOLEAN_P,BOOLEAN_P));
 
 /* ### mapglyph.c ### */
 
