43664aa31011ca74366b7d6ae6bf05257cd3126f
cohrs <cohrs>
2003-11-26 23:42:10 +0000

    interim status support for X11 and gnome so these window ports can compile with STATUS_VIA_WINDOWPORT defined

--- win/gnome/gnbind.c
+++ win/gnome/gnbind.c
@@ -19,6 +19,9 @@ winid WIN_WORN = WIN_ERR;
 extern void tty_raw_print(const char *);
 extern void tty_raw_print_bold(const char *);
 
+/* this is only needed until gnome_status_* routines are written */
+extern NEARDATA winid WIN_STATUS;
+
 
 /* Interface definition, for windows.c */
 struct window_procs Gnome_procs = {
