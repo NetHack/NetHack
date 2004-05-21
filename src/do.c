0414c09c7a658efb1538407c038f12f866264ef5
cohrs <cohrs>
2004-05-21 21:20:16 +0000

    U917 - floating above unknown stairs Attempting to go > while blind and floating over unknown stairs/ladder would refer to the stairs in the "high above" message (however, the stairs/ladder remained unidentified on the map).  Change this case so it only refers to stairs/ladder if that's what the hero remembers on the map.

--- src/do.c
+++ src/do.c
@@ -770,6 +770,18 @@ dodown()
 		if (float_down(I_SPECIAL|TIMEOUT, W_ARTI))
 		    return (1);   /* came down, so moved */
 	    }
+	    if (Blind) {
+		/* Avoid alerting player to an unknown stair or ladder.
+		 * Changes the message for a covered, known staircase
+		 * too; staircase knowledge is not stored anywhere.
+		 */
+		if (stairs_down)
+		    stairs_down =
+			(glyph_to_cmap(levl[u.ux][u.uy].glyph) == S_dnstair);
+		else if (ladder_down)
+		    ladder_down =
+			(glyph_to_cmap(levl[u.ux][u.uy].glyph) == S_dnladder);
+	    }
 	    floating_above(stairs_down ? "stairs" : ladder_down ?
 			   "ladder" : surface(u.ux, u.uy));
 	    return (0);   /* didn't move */
