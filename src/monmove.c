3e3ec4ebe511651d30800698c11dc9e7db560e24
nethack.rankin <nethack.rankin>
2005-06-26 04:02:24 +0000

    fix #M100 - mind flayer's psychic blast during prayer
         From a bug report:  the invulnerability conferred
    during the multi-turn delay for a successful prayer was not protecting
    against damage inflicted by hostile mind flayer's "wave of mental energy".

--- src/monmove.c
+++ src/monmove.c
@@ -404,9 +404,9 @@ register struct monst *mtmp;
 		}
 		pline("A wave of psychic energy pours over you!");
 		if (mtmp->mpeaceful &&
-		    (!Conflict || resist(mtmp, RING_CLASS, 0, 0)))
+		    (!Conflict || resist(mtmp, RING_CLASS, 0, 0))) {
 			pline("It feels quite soothing.");
-		else {
+		} else if (!u.uinvulnerable) {
 			register boolean m_sen = sensemon(mtmp);
 
 			if (m_sen || (Blind_telepat && rn2(2)) || !rn2(10)) {
