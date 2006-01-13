40b426a977d735d04189808a40f12db29e11acaf
nethack.allison <nethack.allison>
2006-01-13 04:47:04 +0000

    tin details at end of game
    Pat Rankin wrote:
    >      Won't the players who notice this immediately ask why they
    > can't see the same information for fully ID'd tins during play?
    
    It wasn't clear to me whether Pat wanted the info shown on
    fully ID'd tins during play, or whether he didn't want the info
    displayed at all.
    
    This withdraws the end-of-game tin details until clarified.

--- src/end.c
+++ src/end.c
@@ -383,8 +383,6 @@ boolean taken;
 			    obj->known = obj->bknown = obj->dknown = obj->rknown = 1;
 			    if (Is_container(obj) || obj->otyp == STATUE)
 				obj->cknown = obj->lknown = 1;
-			    if (obj->otyp == TIN)
-			    	obj->cknown = 1;
 			}
 			(void) display_inventory((char *)0, TRUE);
 			container_contents(invent, TRUE, TRUE);
