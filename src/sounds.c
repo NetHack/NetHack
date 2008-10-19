9a7c5194a0d41e6fe290b8a6b769ae0f45ab6f4f
nethack.rankin <nethack.rankin>
2008-10-19 22:54:01 +0000

    comment tidbit (trunk only)
         Noticed when fixing branch code.

--- src/sounds.c
+++ src/sounds.c
@@ -481,7 +481,7 @@ register struct monst *mtmp;
     /* leader might be poly'd; if he can still speak, give leader speach */
     if (mtmp->m_id == quest_status.leader_m_id && msound > MS_ANIMAL)
 	msound = MS_LEADER;
-    /* make sure its your role's quest quardian; adjust if not */
+    /* make sure it's your role's quest quardian; adjust if not */
     else if (msound == MS_GUARDIAN && ptr != &mons[urole.guardnum])
 	msound = mons[genus(monsndx(ptr), 1)].msound;
     /* some normally non-speaking types can/will speak if hero is similar */
