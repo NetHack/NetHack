3afd0f87fd6ceb96e6c5d33cf5d067baf3a90a15
keni <keni>
2011-12-29 20:06:27 +0000

    at(1) test#1 / defs for suppressing compiler warnings from pline (etc) First at(1) in postcommit.pl test.  Patch: defs for suppressing compiler warnings.

--- include/decl.h
+++ include/decl.h
@@ -334,6 +334,15 @@ E NEARDATA winid WIN_STATUS;
 E NEARDATA winid WIN_MAP, WIN_INVEN;
 E nhwchar toplines[];
 
+/* pline (et al) for a single string argument (suppress compiler warning) */
+#define pline1(cstr) pline("%s", cstr)
+#define Your1(cstr) Your("%s", cstr)
+#define You1(cstr) You("%s", cstr)
+#define verbalize1(cstr) verbalize("%s", cstr)
+#define You_hear1(cstr) You_hear("%s", cstr)
+#define Sprintf1(buf, cstr) Sprintf(buf, "%s", cstr)
+#define panic1(cstr) panic(cstr)
+
 #ifndef TCAP_H
 E struct tc_gbl_data {	/* also declared in tcap.h */
     char *tc_AS, *tc_AE;	/* graphics start and end (tty font swapping) */
