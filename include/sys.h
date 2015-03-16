562fa952ce95695272467ef8f00277735ba93820
PatR <rankin@nethack.org>
2015-03-16 15:28:01 -0700

    allow DEBUGFILES to be overridden via getenv()
    If getenv("DEBUGFILES") yields a value then it takes precedence over
    sysconf.DEBUGFILES or sys.c's #define DEBUGFILES.  (It probably should
    only be controlled via environment since it is not a system-wide
    attribute, but I haven't taken out the SYSCF handling for it.)

--- include/sys.h
+++ include/sys.h
@@ -1,4 +1,4 @@
-/* NetHack 3.5	sys.h	$NHDT-Date: 1426496454 2015/03/16 09:00:54 $  $NHDT-Branch: master $:$NHDT-Revision: 1.12 $ */
+/* NetHack 3.5	sys.h	$NHDT-Date: 1426544796 2015/03/16 22:26:36 $  $NHDT-Branch: master $:$NHDT-Revision: 1.13 $ */
 /* NetHack 3.5	sys.h	$Date: 2012/01/27 20:15:26 $  $Revision: 1.9 $ */
 /* Copyright (c) Kenneth Lorber, Kensington, Maryland, 2008. */
 /* NetHack may be freely redistributed.  See license for details. */
@@ -12,6 +12,11 @@ struct sysopt {
 	char *wizards;
 	char *shellers;	/* like wizards, for ! command (-DSHELL) */
 	char *debugfiles; /* files to show debugplines in. '*' is all. */
+	int env_dbgfl;	/*  1: debugfiles comes from getenv("DEBUGFILES")
+			 *     so sysconf's DEBUGFILES shouldn't override it;
+			 *  0: getenv() hasn't been attempted yet;
+			 * -1: getenv() didn't find a value for DEBUGFILES.
+			 */    
 	int   maxplayers;
 		/* record file */
 	int persmax;
