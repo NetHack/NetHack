dfe967732d181df07a26814848ed4bb285d6781f
Pasi Kallinen <paxed@alt.org>
2015-03-24 21:14:49 +0200

    Add more hallu sounds monsters make when hit

--- src/sounds.c
+++ src/sounds.c
@@ -304,7 +304,11 @@ dosounds()
 
 static const char * const h_sounds[] = {
     "beep", "boing", "sing", "belche", "creak", "cough", "rattle",
-    "ululate", "pop", "jingle", "sniffle", "tinkle", "eep"
+    "ululate", "pop", "jingle", "sniffle", "tinkle", "eep",
+    "clatter", "hum", "sizzle", "twitter", "wheeze", "rustle",
+    "honk", "lisp", "yodel", "coo", "burp", "moo", "boom",
+    "murmur", "oink", "quack", "rumble", "twang", "bellow",
+    "toot", "gargle", "hoot", "warble"
 };
 
 const char *
