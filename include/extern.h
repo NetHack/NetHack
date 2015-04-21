c9bb75eaf38a408a5587772ce220759f7b27b64f
Pasi Kallinen <paxed@alt.org>
2015-04-21 22:40:27 +0300

    Unify dropping boulder on monster

--- include/extern.h
+++ include/extern.h
@@ -1863,6 +1863,7 @@ E void FDECL(forget_levels, (int));
 E void NDECL(forget_traps);
 E void FDECL(forget_map, (int));
 E int FDECL(seffects, (struct obj *));
+E boolean FDECL(drop_boulder_on_monster, (int, int, BOOLEAN_P, BOOLEAN_P));
 E void FDECL(wand_explode, (struct obj *,int));
 #ifdef USE_TRAMPOLI
 E void FDECL(set_lit, (int,int,genericptr_t));
