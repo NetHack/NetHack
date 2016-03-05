83c58f009ed0a6a335983e7d432044c41b3bb85c
nhmall <mjnh@persona.ca>
2016-03-05 15:38:44 -0500

    Another file for support of Ray Chason's patch

---
+++ include/integer.h
@@ -0,0 +1,40 @@
+/* NetHack 3.6	integer.h	$NHDT-Date: 1457210314 2016/03/05 20:38:34 $  $NHDT-Branch: chasonr $:$NHDT-Revision: 1.0 $ */
+/* NetHack may be freely redistributed.  See license for details. */
+
+/* integer.h -- provide sized integer types */
+
+#ifndef INTEGER_H
+#define INTEGER_H
+
+#if defined(__STDC__) && __STDC__ >= 199101L
+
+/* The compiler claims to conform to C99. Use stdint.h */
+#include <stdint.h>
+typedef uint8_t uint8;
+typedef int16_t int16;
+typedef uint16_t uint16;
+typedef int32_t int32;
+typedef uint32_t uint32;
+
+#else /* !C99 */
+
+/* Provide uint8, int16, uint16, int32 and uint32 */
+typedef unsigned char uint8;
+typedef short int16;
+typedef unsigned short uint16;
+
+#if defined(__WATCOMC__) && !defined(__386__)
+/* Open Watcom providing a 16 bit build for MS-DOS or OS/2 */
+/* int is 16 bits; use long for 32 bits */
+typedef long int32;
+typedef unsigned long uint32;
+#else
+/* Otherwise, assume either a 32- or 64-bit compiler */
+/* long may be 64 bits; use int for 32 bits */
+typedef int int32;
+typedef unsigned int uint32;
+#endif
+
+#endif /* !C99 */
+
+#endif /* INTEGER_H */
