16118580d5325b8da0be339f0a6cea333c13ecc3
Pasi Kallinen <paxed@alt.org>
2016-06-20 12:56:55 +0300

    Tame simians with bananas
    Bananas are quite rare, and none of the simians are very strong,
    so this won't be in any way overpowered - just a bit of flavor.
    
    I think this was originally Derek's change from Sporkhack, but
    similar ones have been done in the community multiple times
    in the past.

--- include/mondata.h
+++ include/mondata.h
@@ -194,6 +194,10 @@
      || ((ptr)->mlet == S_PUDDING && (ptr) != &mons[PM_BLACK_PUDDING]))
 
 #define befriend_with_obj(ptr, obj) \
-    ((obj)->oclass == FOOD_CLASS && is_domestic(ptr))
+    ((obj)->oclass == FOOD_CLASS && is_domestic(ptr)                    \
+     || ((obj)->otyp == BANANA &&                                       \
+         ((ptr) == &mons[PM_MONKEY]                                     \
+          || (ptr) == &mons[PM_APE]                                     \
+          || (ptr) == &mons[PM_CARNIVOROUS_APE])))
 
 #endif /* MONDATA_H */
