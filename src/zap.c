14bd550481506798ebf7388f94e960069abfa8af
Pasi Kallinen <paxed@alt.org>
2016-10-15 19:47:04 +0300

    Wielding Trollsbane prevents trolls from reviving

--- src/zap.c
+++ src/zap.c
@@ -748,7 +748,9 @@ boolean by_hero;
             x = xy.x, y = xy.y;
     }
 
-    if (mons[montype].mlet == S_EEL && !IS_POOL(levl[x][y].typ)) {
+    if ((mons[montype].mlet == S_EEL && !IS_POOL(levl[x][y].typ))
+        || (mons[montype].mlet == S_TROLL
+            && uwep && uwep->oartifact == ART_TROLLSBANE)) {
         if (by_hero && cansee(x,y))
             pline("%s twitches feebly.",
                 upstart(corpse_xname(corpse, (const char *) 0, CXN_PFX_THE)));
