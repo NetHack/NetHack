7cb97cbd14ced63f21da3eaf14e66bc89f11625e
karnov <karnov@sympatico.ca>
2016-12-27 23:08:05 -0500

    Modified player name list to include new team members.

--- src/mplayer.c
+++ src/mplayer.c
@@ -16,9 +16,10 @@ STATIC_DCL void FDECL(mk_mplayer_armor, (struct monst *, SHORT_P));
  */
 static const char *developers[] = {
     /* devteam */
-    "Dave",    "Dean",   "Eric",    "Izchak", "Janet",    "Jessie",
-    "Ken",     "Kevin",  "Michael", "Mike",   "Pat",      "Paul",
-    "Steve",   "Timo",   "Warwick",
+    "Alex",    "Dave",   "Dean",    "Derek",   "Eric",    "Izchak",
+    "Janet",   "Jessie", "Ken",     "Kevin",   "Michael", "Mike",
+    "Pasi",    "Pat",    "Patric",  "Paul",    "Sean",    "Steve",
+    "Timo",    "Warwick",
     /* PC team */
     "Bill",    "Eric",   "Keizo",   "Ken",    "Kevin",    "Michael",
     "Mike",    "Paul",   "Stephen", "Steve",  "Timo",     "Yitzhak",
