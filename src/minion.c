5a15b2459e5531ff28ba0a4099cd5588a80ab2d4
Tangles <andyrthomson@gmail.com>
2017-03-23 11:15:45 +1100

    Fixed demon-prince bribe logic accidentally broken with recent livelog change
    Also fixed wording of LL message for bribe.

--- src/minion.c
+++ src/minion.c
@@ -279,11 +279,12 @@ register struct monst *mtmp;
         if ((offer = bribe(mtmp)) >= demand) {
             pline("%s vanishes, laughing about cowardly mortals.",
                   Amonnam(mtmp));
-            livelog_printf(LL_UMONST, "bribes %s with %ld %s for safe passage",
+            livelog_printf(LL_UMONST, "bribed %s with %ld %s for safe passage",
                   Amonnam(mtmp), offer, currency(offer)); 
+        } else if (offer > 0L && (long) rnd(40) > (demand - offer)) {
             pline("%s scowls at you menacingly, then vanishes.",
                   Amonnam(mtmp));
-            livelog_printf(LL_UMONST, "bribes %s with %ld %s for safe passage",
+            livelog_printf(LL_UMONST, "bribed %s with %ld %s for safe passage",
                   Amonnam(mtmp), offer, currency(offer)); 
         } else {
             pline("%s gets angry...", Amonnam(mtmp));
