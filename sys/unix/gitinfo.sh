ddb830911e10d0e1596da4977503b8067e4e8012
PatR <rankin@nethack.org>
2018-03-10 12:32:52 -0800

    more Unix gitinfo
    When make uses 'makedefs -v' to create date.h, force it to create
    gitinfo.txt all the time instead of just when that doesn't already
    exist.  Use 'make GITINFO=0' to get the previous behavior.
    
    To skip it entirely, you need to do that and also make sure that
    some file by that name already exists.  'touch dat/gitinfo.txt' or
    perhaps 'echo "#no git" > dat/gitinfo.txt' would suffice.

--- sys/unix/gitinfo.sh
+++ sys/unix/gitinfo.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # NetHack 3.6  gitinfo.sh	$NHDT-Date: 1520201830 2018/03/04 22:17:10 $  $NHDT-Branch: NetHack-3.6.0 $:$NHDT-Revision: 1.0 $
 
-# if dat/gitinfo.txt doesn't exist, try to make one; called from Makefile.src
+# bring dat/gitinfo.txt up to date; called from Makefile.src
 
 #
 # gitinfo.txt is used during development to augment the version number
@@ -10,13 +10,16 @@
 # this script to be skipped or to run but fail to generate dat/gitinfo.txt.
 #
 
+always=0
+if [[ $1 -eq 1 || $1 == "force" || $1 == "always" ]]; then always=1; fi
+
 # try to figure out where we are: top, one level down (expected), or sys/unix
 prefix=.
 if [ -f ../sys/unix/gitinfo.sh ]; then prefix=..; fi
 if [ -f ../../sys/unix/gitinfo.sh ]; then prefix=../..; fi
 
 # try to run a perl script which is part of nethack's git repository
-if [[ ! -f $prefix/dat/gitinfo.txt ]]; then
+if [[ $always -eq 1 || ! -f $prefix/dat/gitinfo.txt ]]; then
   ( cd $prefix; \
     perl -IDEVEL/hooksdir -MNHgithook -e '&NHgithook::nhversioning' \
       2> /dev/null ) \
