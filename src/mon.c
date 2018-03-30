4eab7b140cee1ac3f50a555f021e0ca8c36c06af
Pasi Kallinen <paxed@alt.org>
2018-03-30 20:34:19 +0300

    Don't store int into unsigned

--- src/mon.c
+++ src/mon.c
@@ -3040,7 +3040,8 @@ struct monst *mon;
 int shiftflags;
 {
     struct permonst *ptr = 0;
-    unsigned mndx, was_female = mon->female;
+    int mndx;
+    unsigned was_female = mon->female;
     boolean msg = FALSE, dochng = FALSE;
 
     if ((shiftflags & SHIFT_MSG)
