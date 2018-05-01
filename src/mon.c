8670624a78797c9982ebc182c64096d396864645
Connor Gregorich-Trevor <gregoric@grinnell.edu>
2018-04-30 19:13:02 -0500

    Lifesaved monsters now reassess their inventory upon resurrection to prevent not equipping amulets or replacing disintegrated armor.

--- src/mon.c
+++ src/mon.c
@@ -1881,6 +1881,9 @@ struct monst *mtmp;
         }
         if (lifesave) {
             m_useup(mtmp, lifesave);
+            /* monsters with multiple amulets / disintegrated items will now
+               re-equip themselves at the cost of a single m_dowear call. */
+            m_dowear(mtmp, FALSE);
         }
 
         surviver = !(mvitals[monsndx(mtmp->data)].mvflags & G_GENOD);
