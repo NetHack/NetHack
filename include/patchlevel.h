a46446725cad940e2231fb6b7d40422ab2179d46
Connor Gregorich-Trevor <gregoric@grinnell.edu>
2018-05-03 22:26:05 -0500

    Updated banner and versioning.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -16,7 +16,7 @@
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
  */
-#define EDITLEVEL 1
+#define EDITLEVEL 2
 
 #define COPYRIGHT_BANNER_A "SpliceHack, Copyright 1985-2018"
 #define COPYRIGHT_BANNER_B \
@@ -33,7 +33,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-#define VERSION_COMPATIBILITY 0x03060002L
+#define VERSION_COMPATIBILITY 0x00010102L
 
 /****************************************************************************/
 /* Version 3.6.x */
