f45f0116c28d0cf63c3d5959bc60ea345cebe4a9
Connor Gregorich-Trevor <gregoric@grinnell.edu>
2018-05-21 18:58:45 -0500

    Incremented version number.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -7,7 +7,7 @@
 
 /* NetHack 3.6.1 */
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 2
+#define VERSION_MINOR 3
 /*
  * PATCHLEVEL is updated for each release.
  */
@@ -16,7 +16,7 @@
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
  */
-#define EDITLEVEL 3
+#define EDITLEVEL 4
 
 #define COPYRIGHT_BANNER_A "SpliceHack, Copyright 1985-2018"
 #define COPYRIGHT_BANNER_B \
@@ -33,7 +33,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-#define VERSION_COMPATIBILITY 0x00020002L
+#define VERSION_COMPATIBILITY 0x00030004L
 
 /****************************************************************************/
 /* Version 3.6.x */
