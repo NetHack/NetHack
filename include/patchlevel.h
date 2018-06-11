edd42c6e6dccf99db7dc5080078c59802240692d
Connor Gregorich-Trevor <gregoric@grinnell.edu>
2018-06-11 16:37:27 -0500

    Incremented patchlevel.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -11,7 +11,7 @@
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL 1
+#define PATCHLEVEL 2
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
@@ -33,7 +33,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-#define VERSION_COMPATIBILITY 0x00030105L
+#define VERSION_COMPATIBILITY 0x00030205L
 
 /****************************************************************************/
 /* Version 3.6.x */
