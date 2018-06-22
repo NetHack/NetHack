54e9becc312297c8427b1304cb7e7a954aeef426
Connor Gregorich-Trevor <gregoric@grinnell.edu>
2018-06-21 19:25:52 -0500

    Added messages for triggering teleportation traps.

--- src/teleport.c
+++ src/teleport.c
@@ -887,6 +887,13 @@ struct trap *trap;
         vault_tele();
     } else
         tele();
+    /* Give the player a message when they trigger a teleport trap.  */
+    if (!In_endgame(&u.uz) && !Antimagic) {
+        if (!Blind)
+            pline("The air around you shimmers!");
+        else
+            You("are briefly feel dizzy.");
+    }
 }
 
 void
