576f47a798725462c8791ef8933d7566b980486f
Connor Gregorich-Trevor <gregoric@grinnell.edu>
2018-07-23 10:07:25 -0500

    Wished-for objects are now always of their default material (xnethack).

--- src/objnam.c
+++ src/objnam.c
@@ -4105,6 +4105,11 @@ typfnd:
                   materialnm[material]);
         }
         otmp->material = material;
+    } else {
+        /* for now, material in wishes will always be base; this is to prevent
+         * problems like wishing for arrows and getting glass arrows which will
+         * shatter. */
+        otmp->material = objects[otmp->otyp].oc_material;
     }
 
 
