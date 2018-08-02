c96d5580e131acc897e59698b4a80db5146e1130
Connor Gregorich-Trevor <gregoric@grinnell.edu>
2018-08-02 09:16:45 -0500

    Fixed invalid function calls for material wish checks.

--- src/objnam.c
+++ src/objnam.c
@@ -3101,8 +3101,8 @@ struct obj *no_wish;
         } else {
             /* check for materials */
             if (!strncmpi(bp, "silver dragon", l = 13)
-                || !strcmpi(bp, "silver ring", l = 11)
-                || !strcmpi(bp, "gold ring", l = 9)
+                || !strncmpi(bp, "silver ring", l = 11)
+                || !strncmpi(bp, "gold ring", l = 9)
                 || !strncmpi(bp, "platinum yendorian express card", l = 31)
                 || !strcmp(bp, "gold")) {
                 /* hack so that silver dragon scales/mail doesn't get
