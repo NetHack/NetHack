0fb263ea881f898e16cf9c6ef347ca5e413b33dc
nhmall <nhmall@nethack.org>
2018-11-05 15:33:02 -0500

    fix broken build cause by missing semicolon

--- src/hack.c
+++ src/hack.c
@@ -2535,9 +2535,10 @@ pickup_checks()
         else if (IS_DOOR(lev->typ) && (lev->doormask & D_ISOPEN))
             pline("It won't come off the hinges.");
         else if (IS_ALTAR(lev->typ))
-            pline("Moving the altar would be a very bad idea.")
+            pline("Moving the altar would be a very bad idea.");
         else if (lev->typ == STAIRS)
-            pline_The("stairs are solidly fixed to the ground.");
+            pline_The("stairs are solidly fixed to the %s.",
+                      surface(u.ux, u.uy));
         else
             There("is nothing here to pick up.");
         return 0;
