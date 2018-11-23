cff9fb441a59d06134b731c57d84fe9c1b2143a0
Bart House <bart@barthouse.com>
2018-11-23 11:27:40 -0800

    define nhassert if not defined by port.

--- include/global.h
+++ include/global.h
@@ -364,5 +364,9 @@ struct savefile_info {
 #define nethack_enter(argc, argv) ((void) 0)
 #endif
 
+/* Supply nhassert macro if not supplied by port */
+#ifndef nhassert
+#define nhassert(expression) ((void)0)
+#endif
 
 #endif /* GLOBAL_H */
