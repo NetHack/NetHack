0fe6a731dff521a7dd1c1a9f3c36d5438d24c22e
PatR <rankin@nethack.org>
2018-12-16 14:21:30 -0800

    fix #H2204 - mkclass() mon selection distribution
    That #H number isn't a typo.  This finally fixes--at least improves--
    something reported eight years ago.  The monster types chosen by
    mkclass() could be way off in some circumstances.  Cited example was
    repeated same-race sacrifice by chaotic hero on dungeon level 20; it
    produced about twice as many incubi as succubi even though they're
    the same as far as difficulty goes.  (No changes in the intervening
    years had any discernable effect; that was still reproducible.)
    The report also mentioned that ndemon() threw away the result from
    mkclass() and retried quite often and suggested that mkclass() be
    taught to filter by alignment when caller cared about that.
    
    This seems to even things out, although it also made harder monsters
    chosen more often.  A test program generated these numbers when
    picking a chaotic demon 10000 times (level 1 hero on dungeon level 20,
    so not realistic; actually probably level 0 hero since the program
    didn't initialize struct u.)  Third column is the number of times the
    monster type was chosen with the old mkclass(), fourth is same for
    the new one.
        mkclass() calls    27315 10000
    286 succubus            2800  3309
    288 incubus             5552  3262
    291 marilith             973   780
    292 vrock                477  1617
    293 hezrou               150   626
    294 bone devil            46   247
    295 ice devil              2   107
    296 nalfeshnee             0    23
    297 pit fiend              0    15
    298 sandestin              0     4
    299 balrog                 0    10
    Note that vrock has generation frequency 2 and marilith only 1, so
    getting twice as many vrocks as mariliths should be expected.
    
    I temporarily changed ndemon() to ask for lawful demons instead of
    chaotic ones and got this.
        mkclass() calls    15762 10000
    287 horned devil        3197  3375
    289 erinys              4991  3339
    290 barbed devil        1812  3286
    
    I also ran it for dragons with any alignment (so the outcome was
    never thrown away; 10000 calls were needed for 10000 picks) instead
    of demons of specific alignment and am suspicious of the outcome.
        mkclass() calls    10000 10000
    140 baby yellow dragon  1124     0
    141 gray dragon         1096  1096
    142 silver dragon       1073  1099
    143 red dragon          1061  1126
    144 white dragon        1077  1128
    145 orange dragon       1141  1118
    146 black dragon        1154  1049
    147 blue dragon         1137  1123
    148 green dragon        1137  1154
    149 yellow dragon          0  1107
    There may be a flaw in the test program.  Or else old mkclass() was
    not very good at picking dragons.

--- src/minion.c
+++ src/minion.c
@@ -1,4 +1,4 @@
-/* NetHack 3.6	minion.c	$NHDT-Date: 1432512773 2015/05/25 00:12:53 $  $NHDT-Branch: master $:$NHDT-Revision: 1.33 $ */
+/* NetHack 3.6	minion.c	$NHDT-Date: 1544998886 2018/12/16 22:21:26 $  $NHDT-Branch: NetHack-3.6.2-beta01 $:$NHDT-Revision: 1.40 $ */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /*-Copyright (c) Robert Patrick Rankin, 2008. */
 /* NetHack may be freely redistributed.  See license for details. */
@@ -256,7 +256,7 @@ register struct monst *mtmp;
               flags.female ? "Sister" : "Brother");
         if (!tele_restrict(mtmp))
             (void) rloc(mtmp, TRUE);
-        return (1);
+        return 1;
     }
     cash = money_cnt(invent);
     demand =
@@ -292,7 +292,7 @@ register struct monst *mtmp;
         }
     }
     mongone(mtmp);
-    return (1);
+    return 1;
 }
 
 long
@@ -323,7 +323,7 @@ struct monst *mtmp;
     }
     (void) money2mon(mtmp, offer);
     context.botl = 1;
-    return (offer);
+    return offer;
 }
 
 int
@@ -336,9 +336,9 @@ aligntyp atyp;
         pm = rn1(PM_DEMOGORGON + 1 - PM_ORCUS, PM_ORCUS);
         if (!(mvitals[pm].mvflags & G_GONE)
             && (atyp == A_NONE || sgn(mons[pm].maligntyp) == sgn(atyp)))
-            return (pm);
+            return pm;
     }
-    return (dlord(atyp)); /* approximate */
+    return dlord(atyp); /* approximate */
 }
 
 int
@@ -351,9 +351,9 @@ aligntyp atyp;
         pm = rn1(PM_YEENOGHU + 1 - PM_JUIBLEX, PM_JUIBLEX);
         if (!(mvitals[pm].mvflags & G_GONE)
             && (atyp == A_NONE || sgn(mons[pm].maligntyp) == sgn(atyp)))
-            return (pm);
+            return pm;
     }
-    return (ndemon(atyp)); /* approximate */
+    return ndemon(atyp); /* approximate */
 }
 
 /* create lawful (good) lord */
@@ -361,9 +361,9 @@ int
 llord()
 {
     if (!(mvitals[PM_ARCHON].mvflags & G_GONE))
-        return (PM_ARCHON);
+        return PM_ARCHON;
 
-    return (lminion()); /* approximate */
+    return lminion(); /* approximate */
 }
 
 int
@@ -375,7 +375,7 @@ lminion()
     for (tryct = 0; tryct < 20; tryct++) {
         ptr = mkclass(S_ANGEL, 0);
         if (ptr && !is_lord(ptr))
-            return (monsndx(ptr));
+            return monsndx(ptr);
     }
 
     return NON_PM;
@@ -383,19 +383,26 @@ lminion()
 
 int
 ndemon(atyp)
-aligntyp atyp;
+aligntyp atyp; /* A_NONE is used for 'any alignment' */
 {
-    int tryct;
     struct permonst *ptr;
 
-    for (tryct = 0; tryct < 20; tryct++) {
-        ptr = mkclass(S_DEMON, 0);
-        if (ptr && is_ndemon(ptr)
-            && (atyp == A_NONE || sgn(ptr->maligntyp) == sgn(atyp)))
-            return (monsndx(ptr));
-    }
-
-    return NON_PM;
+    /*
+     * 3.6.2:  [fix #H2204, 22-Dec-2010, eight years later...]
+     * pick a correctly aligned demon in one try.  This used to
+     * use mkclass() to choose a random demon type and keep trying
+     * (up to 20 times) until it got one with the desired alignment.
+     * mkclass_aligned() skips wrongly aligned potential candidates.
+     * [The only neutral demons are djinni and mail daemon and
+     * mkclass() won't pick them, but call it anyway in case either
+     * aspect of that changes someday.]
+     */
+#if 0
+    if (atyp == A_NEUTRAL)
+        return NON_PM;
+#endif
+    ptr = mkclass_aligned(S_DEMON, 0, atyp);
+    return (ptr && is_ndemon(ptr)) ? monsndx(ptr) : NON_PM;
 }
 
 /* guardian angel has been affected by conflict so is abandoning hero */
