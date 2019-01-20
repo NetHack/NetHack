77bc07f579290eefbfeb946a786e73715fcd21bd
Pasi Kallinen <paxed@alt.org>
2019-01-20 15:56:40 +0200

    Make demon gating show a message
    This was both an accessibility and comprehensibility issue.

--- src/minion.c
+++ src/minion.c
@@ -143,6 +143,8 @@ struct monst *mon;
                 EMIN(mtmp)->renegade =
                     (atyp != u.ualign.type) ^ !mtmp->mpeaceful;
             }
+            if (is_demon(ptr) && canseemon(mtmp))
+                pline("%s appears in a cloud of smoke!", Amonnam(mtmp));
         }
         cnt--;
     }
