647a7f365637a58637771664eebb6afb72ac84d7
copperwater <aosdict@gmail.com>
2019-02-01 13:02:00 -0500

    Confused scrolls of earth summon earthy monsters
    This is another SliceHack feature, but again heavily modified. This gets
    rid of the current pointless confused effect of dropping rocks instead
    of boulders; earth elementals and dust vortices get created instead.
    
    Unlike SliceHack, there is no possibility of causing an earthquake;
    instead, varying the beatitude varies the number of monsters created
    instead. Also unlike SliceHack, this doesn't really care about
    difficulty and won't go out of its way to guarantee certain numbers of
    vortices or elementals; the magic of the scroll is what it is.
    Furthermore, the generated monsters aren't forced to be hostile.
    (Vortices are always hostile anyway; earth elementals might be peaceful
    for neutrals, and if so, good for them.)

--- src/read.c
+++ src/read.c
@@ -1570,7 +1570,16 @@ struct obj *sobj; /* scroll, or fake spellbook object for scroll-like spell */
     }
     case SCR_EARTH:
         /* TODO: handle steeds */
-        if (!Is_rogue_level(&u.uz) && has_ceiling(&u.uz)
+        if (confused) {
+            /* create earth elementals and dust vortices */
+            int i;
+            for (i = 0; i < (2 * (2 + sblessed - scursed)) - 1; ++i) {
+                makemon(&mons[(rn2(3) ? PM_EARTH_ELEMENTAL : PM_DUST_VORTEX)],
+                        u.ux, u.uy, MM_NOWAIT);
+            }
+            pline("The earth moves around you!");
+        }
+        else if (!Is_rogue_level(&u.uz) && has_ceiling(&u.uz)
             && (!In_endgame(&u.uz) || Is_earthlevel(&u.uz))) {
             register int x, y;
             int nboulders = 0;
@@ -1594,13 +1603,13 @@ struct obj *sobj; /* scroll, or fake spellbook object for scroll-like spell */
                             && !IS_AIR(levl[x][y].typ)
                             && (x != u.ux || y != u.uy)) {
                             nboulders +=
-                                drop_boulder_on_monster(x, y, confused, TRUE);
+                                drop_boulder_on_monster(x, y, FALSE, TRUE);
                         }
                     }
                 }
             /* Attack the player */
             if (!sblessed) {
-                drop_boulder_on_player(confused, !scursed, TRUE, FALSE);
+                drop_boulder_on_player(FALSE, !scursed, TRUE, FALSE);
             } else if (!nboulders)
                 pline("But nothing else happens.");
         }
