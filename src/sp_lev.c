cc1219b5a8bae222a6ba6a0c755419c9ca200a79
Bart House <bart@barthouse.com>
2019-07-14 14:02:05 -0700

    Revert type change of lit which would break code.
    Cast lit (which will be 1) to unsigned int to remove warning.

--- src/sp_lev.c
+++ src/sp_lev.c
@@ -3763,7 +3763,7 @@ struct opvar *mc;
 {
     int x, y;
     schar mapc;
-    uchar lit;
+    xchar lit;
     struct opvar *ret = selection_opvar((char *) 0);
 
     if (!ov || !mc || !ret)
@@ -3783,7 +3783,7 @@ struct opvar *mc;
                     break;
                 case 0:
                 case 1:
-                    if (levl[x][y].lit == lit)
+                    if (levl[x][y].lit == (unsigned int) lit)
                         selection_setpoint(x, y, ret, 1);
                     break;
                 }
