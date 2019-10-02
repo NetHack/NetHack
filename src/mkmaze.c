1df652683e77a97b451361a181d7b3f0465c0c37
nhmall <nhmall@nethack.org>
2019-10-01 21:06:07 -0400

    allow orctown booty items to be initialized in mksobj

--- src/mkmaze.c
+++ src/mkmaze.c
@@ -702,7 +702,7 @@ const char *gang;
 {
     struct obj *otmp;
 
-    otmp = mksobj_migr_to_species(otyp, (unsigned long) M2_ORC, FALSE, FALSE);
+    otmp = mksobj_migr_to_species(otyp, (unsigned long) M2_ORC, TRUE, FALSE);
     if (otmp && gang) {
         new_oname(otmp, strlen(gang) + 1); /* removes old name if present */
         Strcpy(ONAME(otmp), gang);
