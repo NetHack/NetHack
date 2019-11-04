93f95dd05146e1c6c9093b42be786f1b0b5182ce
nhmall <nhmall@nethack.org>
2019-11-04 00:36:54 -0500

    more deafness-related message corrections

--- src/shk.c
+++ src/shk.c
@@ -2989,8 +2989,9 @@ boolean peaceful, silent;
             if (canseemon(shkp)) {
                 Norep("%s booms: \"%s, you are a thief!\"",
                       Shknam(shkp), plname);
-            } else
+            } else if (!Deaf) {
                 Norep("You hear a scream, \"Thief!\"");
+            }
         }
         hot_pursuit(shkp);
         (void) angry_guards(FALSE);
