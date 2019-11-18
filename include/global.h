e85646816cec5ae749bb564f0617425c6dead620
nhmall <nhmall@nethack.org>
2019-11-17 20:06:21 -0500

    mark source tree as beta, previously marked work-in-progress

--- include/global.h
+++ include/global.h
@@ -18,7 +18,7 @@
 /*
  * Development status of this NetHack version.
  */
-#define NH_DEVEL_STATUS NH_STATUS_WIP
+#define NH_DEVEL_STATUS NH_STATUS_BETA
 
 #ifndef DEBUG  /* allow tool chains to define without causing warnings */
 #define DEBUG
