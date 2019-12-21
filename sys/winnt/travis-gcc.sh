22d2a8c4671374cd28c235327b5ff8e4bb061ef3
nhmall <nhmall@nethack.org>
2019-12-21 13:31:08 -0500

    test mingw with new shell script

---
+++ sys/winnt/travis-gcc.sh
@@ -0,0 +1,12 @@
+set -x
+mkdir -p lib
+cd lib
+git clone --depth 1 https://github.com/wmcbrine/PDCurses.git pdcurses
+git clone --depth 1 https://github.com/universal-ctags/ctags.git ctags
+curl -R -O http://www.lua.org/ftp/lua-5.3.5.tar.gz
+tar zxf lua-5.3.5.tar.gz
+cd ../
+export ADD_CURSES=Y
+export PDCURSES_TOP=../lib/pdcurses
+export
+cd src
