209fab138bed81d66da2bce012d8e0b5e38cebe4
nhmall <nhmall@nethack.org>
2020-01-29 09:15:55 -0500

    recover fix
    recover had deviated somewhat from NetHack in its
    file expectations:
    1) A couple of 3.7 fields needed to be accommodated.
    2) hard-coded file size values had deviated.
    
    The file sizes are now in an added header file named "filesize.h",
    which is included at the bottom of config.h.
    
    There will likely be another commit to write the filename size ahead
    of the file name so that the precise number of characters can be read,
    but since that will break existing saves, it can go in along with another
    save-breaking commit.
    
    This commit doesn't not alter savefiles written by nethack so does not
    require an editlevel bump. It does alter the read-in expectation in
    recover to match the game and this get recover working again.

--- include/config.h
+++ include/config.h
@@ -579,5 +579,6 @@ typedef unsigned char uchar;
 #endif
 
 #include "global.h" /* Define everything else according to choices above */
+#include "filesize.h" /* Define file sizes shared between nethack and recover */
 
 #endif /* CONFIG_H */
