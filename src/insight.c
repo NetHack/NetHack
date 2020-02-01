10b8356a15112d012a72be0a2d1ba3224e391ab8
PatR <rankin@nethack.org>
2020-02-01 09:14:18 -0800

    splitting cmd.c, phase 1B
    I neglected to use -f with 'git add' and the stub file didn't make
    it into the previous commit.

---
+++ src/insight.c
@@ -0,0 +1,10 @@
+/* NetHack 3.7	insight.c	$NHDT-Date: 1580577249 2020/02/01 17:14:09 $  $NHDT-Branch: NetHack-3.7 $:$NHDT-Revision: 1.0 $ */
+/* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
+/* NetHack may be freely redistributed.  See license for details. */
+
+#include "hack.h"
+
+/* temporary, until Makefiles and IDE project files are all updated */
+int insight_dummy = 0;
+
+/*insight.c*/
