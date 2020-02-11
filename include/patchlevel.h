1566063b4cc8abef77943a917c7b642f35ff6dcc
nhmall <nhmall@nethack.org>
2020-02-10 22:12:38 -0500

    bump editlevel
    the code in pr298 altered the sound bit for one existing monster

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -14,7 +14,7 @@
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
  */
-#define EDITLEVEL 14
+#define EDITLEVEL 15
 
 #define COPYRIGHT_BANNER_A "NetHack, Copyright 1985-2020"
 #define COPYRIGHT_BANNER_B \
