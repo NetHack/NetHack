e56d3414a0ec201853890e7b1428ce13527966fc
Pasi Kallinen <paxed@alt.org>
2020-02-24 19:08:58 +0200

    More noflip special levels

--- dat/Pri-loca.lua
+++ dat/Pri-loca.lua
@@ -5,7 +5,7 @@
 --
 des.level_init({ style = "solidfill", fg = " " });
 
-des.level_flags("mazelevel", "hardfloor")
+des.level_flags("mazelevel", "hardfloor", "noflip")
 -- This is a kludge to init the level as a lit field.
 des.level_init({ style="mines", fg=".", bg=".", smoothed=false, joined=false, lit=1, walled=false })
 
--- dat/Sam-fila.lua
+++ dat/Sam-fila.lua
@@ -5,7 +5,7 @@
 --
 des.level_init({ style = "solidfill", fg = " " });
 
-des.level_flags("mazelevel");
+des.level_flags("mazelevel", "noflip");
 
 des.level_init({ style="mines", fg=".", bg="P", smoothed=true, joined=true, walled=true })
 
