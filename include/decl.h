aa3a859befa038208da8347c1791eb0e37b578a3
nhmall <nhmall@nethack.org>
2020-05-06 12:44:12 -0400

    remove obsolete global variable declaration

--- include/decl.h
+++ include/decl.h
@@ -323,8 +323,6 @@ E struct tc_gbl_data {   /* also declared in tcap.h */
 #define CO g.tc_gbl_data.tc_CO
 #endif
 
-E const char *const oclass_names[];
-
 /* Some systems want to use full pathnames for some subsets of file names,
  * rather than assuming that they're all in the current directory.  This
  * provides all the subclasses that seem reasonable, and sets up for all
