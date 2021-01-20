57181c5655112f5989a1e35586994bda731ae720
Pasi Kallinen <paxed@alt.org>
2021-01-20 19:19:40 +0200

    Remove Aztec C workaround from core
    Aztec C is closed-source and dead for more than 20 years.

--- src/sounds.c
+++ src/sounds.c
@@ -27,9 +27,6 @@ dosounds()
 {
     register struct mkroom *sroom;
     register int hallu, vx, vy;
-#if defined(AMIGA) && defined(AZTEC_C_WORKAROUND)
-    int xx;
-#endif
     struct monst *mtmp;
 
     if (Deaf || !flags.acoustics || u.uswallow || Underwater)
@@ -96,16 +93,8 @@ dosounds()
                     for (vy = sroom->ly; vy <= sroom->hy; vy++)
                         if (g_at(vx, vy))
                             gold_in_vault = TRUE;
-#if defined(AMIGA) && defined(AZTEC_C_WORKAROUND)
-                /* Bug in aztec assembler here. Workaround below */
-                xx = ROOM_INDEX(sroom) + ROOMOFFSET;
-                xx = (xx != vault_occupied(u.urooms));
-                if (xx)
-#else
                 if (vault_occupied(u.urooms)
-                    != (ROOM_INDEX(sroom) + ROOMOFFSET))
-#endif /* AZTEC_C_WORKAROUND */
-                {
+                    != (ROOM_INDEX(sroom) + ROOMOFFSET)) {
                     if (gold_in_vault)
                         You_hear(!hallu
                                      ? "someone counting gold coins."
