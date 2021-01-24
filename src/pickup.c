ee7664684a62361d7b597375b948f8d237437cd6
copperwater <aosdict@gmail.com>
2021-01-24 16:39:33 -0500

    Unify code for extracting an object from a monster's inventory
    The code for doing this (basically an obj_extract_self() call plus
    handling if the object was worn or wielded) was duplicated all over, and
    inconsistent - for instance, though all of them updated the monster's
    misc_worn_check to indicate it was no longer wearing something in
    whatever slot, only one call also set the bit that flags the monster to
    consider putting on other gear afterwards.
    
    Under a new function, extract_from_minvent, all this extra handling is
    checked in one function, which can simply replace the obj_extract_self
    call.
    
    A few callers (such as stealing) have some common code *after* the
    object is extracted and some other things happen such as message
    printing, such as calling mselftouch if the object was worn
    gloves. extract_from_minvent does not handle these cases.

--- src/pickup.c
+++ src/pickup.c
@@ -2104,8 +2104,6 @@ loot_mon(struct monst *mtmp, int *passed_info, boolean *prev_loot)
      *  *prev_loot is set to TRUE if something was actually acquired in here.
      */
     if (mtmp && mtmp != u.usteed && (otmp = which_armor(mtmp, W_SADDLE))) {
-        long unwornmask;
-
         if (passed_info)
             *passed_info = 1;
         Sprintf(qbuf, "Do you want to remove the saddle from %s?",
@@ -2123,12 +2121,7 @@ loot_mon(struct monst *mtmp, int *passed_info, boolean *prev_loot)
                 /* the attempt costs you time */
                 return 1;
             }
-            obj_extract_self(otmp);
-            if ((unwornmask = otmp->owornmask) != 0L) {
-                mtmp->misc_worn_check &= ~unwornmask;
-                otmp->owornmask = 0L;
-                update_mon_intrinsics(mtmp, otmp, FALSE, FALSE);
-            }
+            extract_from_minvent(mtmp, otmp, TRUE, FALSE);
             otmp = hold_another_object(otmp, "You drop %s!", doname(otmp),
                                        (const char *) 0);
             nhUse(otmp);
