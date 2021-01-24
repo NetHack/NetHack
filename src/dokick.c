7e700ef627f59a7f36fce056f74acaca1e60ac41
Michael Lehotay <mlehotay@gmail.com>
2021-01-23 23:17:05 -0500

    make appropriately gendered dishwashers

--- src/dokick.c
+++ src/dokick.c
@@ -1159,6 +1159,7 @@ dokick()
         }
         if (IS_SINK(g.maploc->typ)) {
             int gend = poly_gender();
+            unsigned prefs, mgend;
 
             if (Levitation)
                 goto dumb;
@@ -1183,11 +1184,22 @@ dokick()
                 return 1;
             } else if (!(g.maploc->looted & S_LDWASHER) && !rn2(3)
                        && !(g.mvitals[PM_AMOROUS_DEMON].mvflags & G_GONE)) {
+                prefs = (gend == FEMALE) ? (flags.consent_given & 0x0c)>>2 :
+                                           (flags.consent_given & 0x03);
+                switch(gend) {
+                case 0: /* if male */
+                case 1: /* or female */
+                    if (prefs==1 || prefs==2) { /* if straight or gay */
+                        mgend = (prefs - 1) ? MM_FEMALE : MM_MALE;
+                        break;
+                    } /* else fall through */
+                case 2:
+                    mgend = rn2(2) ? MM_FEMALE : MM_MALE;
+                }
+
                 /* can't resist... */
                 pline("%s returns!", (Blind ? Something : "The dish washer"));
-                if (makemon(&mons[PM_AMOROUS_DEMON], x, y,
-                            (gend == 1 || (gend == 2 && rn2(2)))
-                                  ? MM_MALE : MM_FEMALE))
+                if (makemon(&mons[PM_AMOROUS_DEMON], x, y, mgend))
                     newsym(x, y);
                 g.maploc->looted |= S_LDWASHER;
                 exercise(A_DEX, TRUE);
