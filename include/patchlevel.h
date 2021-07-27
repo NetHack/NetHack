d33272033600053a876a08e13d385f3225967570
Pasi Kallinen <paxed@alt.org>
2021-07-27 18:56:46 +0300

    Increment editlevel
    Added the new branch stairs glyphs, forgot to increment editlevel.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -17,7 +17,7 @@
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
  */
-#define EDITLEVEL 36
+#define EDITLEVEL 37
 
 /*
  * Development status possibilities.
