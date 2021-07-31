5a7f7289149279e97ba468d152b101fa149d3cf1
Guillaume Clemens <prdrdecroly@gmail.com>
2021-07-31 11:08:12 +0200

    staying under 80 characters, oops

--- src/zap.c
+++ src/zap.c
@@ -5002,7 +5002,8 @@ destroy_one_item(struct obj *obj, int osym, int dmgtyp)
         quan = obj->quan;
         switch (osym) {
         case RING_CLASS:
-            if (obj->otyp == RIN_SHOCK_RESISTANCE || (obj->owornmask && uarmg && !is_metallic(uarmg))) {
+            if (obj->otyp == RIN_SHOCK_RESISTANCE ||
+                    (obj->owornmask && uarmg && !is_metallic(uarmg))) {
                 skip++;
                 break;
             }
