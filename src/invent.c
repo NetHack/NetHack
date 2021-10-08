017446636b87dbe1ebb9bab544881a89196d629f
HMM <mcdonohe@mail.gvsu.edu>
2021-10-08 14:05:05 -0400

    Merge item stacks regardless of bknown

--- src/invent.c
+++ src/invent.c
@@ -715,6 +715,11 @@ merged(struct obj **potmp, struct obj **pobj)
         /* and puddings!!!1!!one! */
         else if (!Is_pudding(otmp))
             otmp->owt += obj->owt;
+
+        /* update bknown */
+        if (otmp->oclass != COIN_CLASS)
+            otmp->bknown = obj->bknown = otmp->bknown || obj->bknown;
+
         if (!has_oname(otmp) && has_oname(obj))
             otmp = *potmp = oname(otmp, ONAME(obj));
         obj_extract_self(obj);
@@ -3647,7 +3652,6 @@ mergable(register struct obj *otmp, register struct obj *obj)
         return FALSE;
 
     if (obj->dknown != otmp->dknown
-        || (obj->bknown != otmp->bknown && !Role_if(PM_CLERIC))
         || obj->oeroded != otmp->oeroded || obj->oeroded2 != otmp->oeroded2
         || obj->greased != otmp->greased)
         return FALSE;
