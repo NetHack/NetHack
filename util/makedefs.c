9b78ff740d84b0fd5b584723f681fb129d2ab131
nhmall <nhmall@nethack.org>
2021-10-21 11:53:01 -0400

    clang 12 warning
    makedefs.c:1560:5: warning: suspicious concatenation of string literals in an array initialization; did you mean to separate the elements with a comma? [-Wstring-concatenation]
        "  ",
        ^
    makedefs.c:1559:5: note: place parentheses around the string literal to silence warning
        "currently available will not suffice for proving that P != NP or        "
        ^
    1 warning generated.

--- util/makedefs.c
+++ util/makedefs.c
@@ -1552,12 +1552,15 @@ h_filter(char *line)
 
 static const char *special_oracle[] = {
     "\"...it is rather disconcerting to be confronted with the",
-    "following theorem from [Baker, Gill, and Solovay, 1975].", "",
+    "following theorem from [Baker, Gill, and Solovay, 1975].",
+    "",
     "Theorem 7.18  There exist recursive languages A and B such that",
-    "  (1)  P(A) == NP(A), and", "  (2)  P(B) != NP(B)", "",
+    "  (1)  P(A) == NP(A), and",
+    "  (2)  P(B) != NP(B)",
+    "",
     "This provides impressive evidence that the techniques that are",
-    "currently available will not suffice for proving that P != NP or        "
-    "  ",
+    ("currently available will not suffice for proving that P != NP or"
+    "          "),
     "that P == NP.\"  [Garey and Johnson, p. 185.]"
 };
 
