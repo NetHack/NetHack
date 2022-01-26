13ee03be1fc65e05f7738607464ef1841a56d363
nhmall <nhmall@nethack.org>
2022-01-26 17:24:59 -0500

    support for build with current Lua version 5.4.4
    On some platforms this may require:
           re-using a sys/unix/hints file to generate updated Makefiles
           make spotless
           make fetch-lua

--- sys/windows/travis-gcc.sh
+++ sys/windows/travis-gcc.sh
@@ -3,6 +3,6 @@ mkdir -p lib
 cd lib
 git clone --depth 1 https://github.com/wmcbrine/PDCurses.git pdcurses
 #git clone --depth 1 https://github.com/universal-ctags/ctags.git ctags
-curl -R -O http://www.lua.org/ftp/lua-5.4.3.tar.gz
-tar zxf lua-5.4.3.tar.gz
+curl -R -O http://www.lua.org/ftp/lua-5.4.4.tar.gz
+tar zxf lua-5.4.4.tar.gz
 cd ../
