01209cab7a047c2a43ece9b56e124064cb496faa
PatR <rankin@nethack.org>
2022-07-21 00:54:51 -0700

    some new feedback for #timeout
    Add a couple of non-timer, non-(property & TIMEOUT) timeout values
    for the wizard-mode #timeout command: uswldtim and uinvault.  The
    swallowed counter goes down and explusion or total digestion occurs
    when it hits zero.  The in-vault counter goes up when you're in a
    vault or the temporary corridor.  If you make it out of the vault-
    and-corridor it gets reset to zero.

--- src/timeout.c
+++ src/timeout.c
@@ -1,4 +1,4 @@
-/* NetHack 3.7	timeout.c	$NHDT-Date: 1653507043 2022/05/25 19:30:43 $  $NHDT-Branch: NetHack-3.7 $:$NHDT-Revision: 1.139 $ */
+/* NetHack 3.7	timeout.c	$NHDT-Date: 1658390077 2022/07/21 07:54:37 $  $NHDT-Branch: NetHack-3.7 $:$NHDT-Revision: 1.142 $ */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /*-Copyright (c) Robert Patrick Rankin, 2018. */
 /* NetHack may be freely redistributed.  See license for details. */
@@ -1906,6 +1906,18 @@ wiz_timeout_queue(void)
                 putstr(win, 0, buf);
             }
         }
+    }
+    if (u.uswldtim) {
+        putstr(win, 0, "");
+        /* decremented when engulfer makes a move, so can last longer than
+           the number of turns reported if engulfer is slow */
+        Sprintf(buf, "Swallow countdown is %u.", u.uswldtim);
+        putstr(win, 0, buf);
+    }
+    if (u.uinvault) {
+        putstr(win, 0, "");
+        Sprintf(buf, "Vault counter is %d.", u.uinvault);
+        putstr(win, 0, buf);
     }
     display_nhwindow(win, FALSE);
     destroy_nhwindow(win);
