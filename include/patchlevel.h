75abc65bab4e26aba287e26e00a3c17828371996
PatR <rankin@nethack.org>
2022-07-23 23:14:12 -0700

    pull request #817 - merge scimitar skill w/ saber
    Pull request from Kufat:  combine a pair of single item skills into
    one skill by changing scimitar to use saber skill and removing no
    longer used scimitar skill.
    
    I don't think skill values end up in save files but decided to
    increment EDITLEVEL to be safe.
    
    Closes #817

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -17,7 +17,7 @@
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
  */
-#define EDITLEVEL 61
+#define EDITLEVEL 62
 
 /*
  * Development status possibilities.
