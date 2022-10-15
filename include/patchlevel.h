46f2903d43f524ce3a2c4f89cedba2cdaa71b483
PatR <rankin@nethack.org>
2022-10-15 06:22:18 -0700

    missing EDITLEVEL update
    The Elbereth fix changed 'struct engr' and should have included an
    increment to EDITLEVEL to invalidate old save and bones files.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -17,7 +17,7 @@
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
  */
-#define EDITLEVEL 65
+#define EDITLEVEL 66
 
 /*
  * Development status possibilities.
