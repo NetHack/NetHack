5ca4e166b1badc83e26650d6a5f5b43642f431d6
Pasi Kallinen <paxed@alt.org>
2023-03-18 10:46:10 +0200

    More fuzzer branch teleporting

--- src/dog.c
+++ src/dog.c
@@ -490,8 +490,9 @@ mon_arrive(struct monst *mtmp, int when)
         if (t) {
             xlocale = t->tx, ylocale = t->ty;
             break;
-        } else if (iflags.debug_fuzzer && (stway = stairway_find_dir(TRUE)) != 0) {
-            /* debugfuzzer returning from knox */
+        } else if (iflags.debug_fuzzer
+                   && (stway = stairway_find_dir(!builds_up(&u.uz))) != 0) {
+            /* debugfuzzer returns from or enters another branch */
             xlocale = stway->sx, ylocale = stway->sy;
         } else if (!(u.uevent.qexpelled
                      && (Is_qstart(&u.uz0) || Is_qstart(&u.uz)))) {
