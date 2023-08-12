7ea091048c7a99919386acc4b83e0181a67d77fa
PatR <rankin@nethack.org>
2023-08-12 15:01:02 -0700

    count_level_features() bit
    The relatively recent code to recalculate the number of fountains and
    sinks on a level was including column 0.  levl[*][0] is not used for
    the map.

--- src/mklev.c
+++ src/mklev.c
@@ -1,4 +1,4 @@
-/* NetHack 3.7	mklev.c	$NHDT-Date: 1689629245 2023/07/17 21:27:25 $  $NHDT-Branch: NetHack-3.7 $:$NHDT-Revision: 1.154 $ */
+/* NetHack 3.7	mklev.c	$NHDT-Date: 1691877661 2023/08/12 22:01:01 $  $NHDT-Branch: NetHack-3.7 $:$NHDT-Revision: 1.155 $ */
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /*-Copyright (c) Alex Smith, 2017. */
 /* NetHack may be freely redistributed.  See license for details. */
@@ -739,7 +739,7 @@ count_level_features(void)
 
     gl.level.flags.nfountains = gl.level.flags.nsinks = 0;
     for (y = 0; y < ROWNO; y++)
-        for (x = 0; x < COLNO; x++) {
+        for (x = 1; x < COLNO; x++) {
             int typ = levl[x][y].typ;
 
             if (typ == FOUNTAIN)
