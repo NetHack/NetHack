b25512f7a1ba9ba06e3731cb78ead754a69d0940
nhmall <nhmall@nethack.org>
2023-09-16 20:02:11 -0400

    fix broken build as of 2a5f03a3
    gcc-9 -g -I../include -DNOTPARMDECL -Wall -Wextra -Wno-missing-field-initializers -Wreturn-type -Wunused -Wformat -Wswitch -Wshadow -Wwrite-strings -pedantic -Wmissing-declarations -Wformat-nonliteral -Wunreachable-code -Wimplicit -Wimplicit-function-declaration -Wimplicit-int -Wmissing-prototypes -Wold-style-definition -Wstrict-prototypes -Wformat-overflow -Wmissing-parameter-type -fPIC -DCURSES_GRAPHICS -D_XOPEN_SOURCE_EXTENDED=1 -DX11_GRAPHICS -DQT_GRAPHICS -DSND_LIB_QTSOUND -DUSER_SOUNDS -DSND_SOUNDEFFECTS_AUTOMAP -DDLB -DHACKDIR=\"/home/vsts/nh/install/games/lib/nethackdir\" -DDEFAULT_WINDOW_SYS=\"tty\" -DSYSCF -DSYSCF_FILE=\"/home/vsts/nh/install/games/lib/nethackdir/sysconf\" -DSECURE -DTIMED_DELAY -DDUMPLOG -DCONFIG_ERROR_SECURE=FALSE -DCOMPRESS=\"/bin/gzip\" -DCOMPRESS_EXTENSION=\".gz\" -DCURSES_UNICODE -DUSE_XPM  -c -o decl.o decl.c
    decl.c:223:15: error: incompatible types when initializing type ‘enum statusfields’ using type ‘void *’
      223 |             , UNDEFINED_PTR, UNDEFINED_PTR
          |               ^~~~~~~~~~~~~
    decl.c:219:44: warning: missing braces around initializer [-Wmissing-braces]
      219 | const struct instance_globals_b g_init_b = {
          |                                            ^
    decl.c:219:44: warning: missing braces around initializer [-Wmissing-braces]
    decl.c:219:44: warning: missing braces around initializer [-Wmissing-braces]
    decl.c:219:44: warning: missing braces around initializer [-Wmissing-braces]
    decl.c:219:44: warning: missing braces around initializer [-Wmissing-braces]
    decl.c:219:44: warning: missing braces around initializer [-Wmissing-braces]
    make[1]: *** [Makefile:1837: decl.o] Error 1

--- src/decl.c
+++ src/decl.c
@@ -218,7 +218,7 @@ const struct instance_globals_a g_init_a = {
 
 const struct instance_globals_b g_init_b = {
     /* botl.c */
-    { { { NULL, NULL, 0L, FALSE, FALSE, 0, 0U, { 0 }, NULL, 0, 0, 0
+    { { { NULL, NULL, 0L, FALSE, FALSE, 0, 0U, { 0 }, { 0 }, NULL, 0, 0, 0
 #ifdef STATUS_HILITES
             , UNDEFINED_PTR, UNDEFINED_PTR
 #endif
