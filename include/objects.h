90ec36bcd64b92d0f5e927c6bbcf618457a61a67
Pasi Kallinen <paxed@alt.org>
2023-10-04 21:43:25 +0300

    Reduce land mine weight
    They were simply too heavy to lug around; now they weigh
    the same beartraps.

--- include/objects.h
+++ include/objects.h
@@ -940,7 +940,7 @@ TOOL("figurine",            NoDes, 1, 0, 1, 0, 25, 50, 80, MINERAL, HI_MINERAL,
 TOOL("magic marker",        NoDes, 1, 0, 1, 1, 15,  2, 50, PLASTIC, CLR_RED,
                                                                 MAGIC_MARKER),
 /* traps */
-TOOL("land mine",           NoDes, 1, 0, 0, 0, 0, 300,180, IRON, CLR_RED,
+TOOL("land mine",           NoDes, 1, 0, 0, 0, 0, 200,180, IRON, CLR_RED,
                                                                 LAND_MINE),
 TOOL("beartrap",            NoDes, 1, 0, 0, 0, 0, 200, 60, IRON, HI_METAL,
                                                                 BEARTRAP),
