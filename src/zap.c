738c2239c9063cbf62e5650977da45c1e38a3108
Pasi Kallinen <paxed@alt.org>
2023-11-04 13:02:57 +0200

    Lightning has a small chance of melting iron bars

--- src/zap.c
+++ src/zap.c
@@ -5143,11 +5143,16 @@ zap_over_floor(
         (void) create_gas_cloud(x, y, 1, 8);
         break;
 
+    case ZT_LIGHTNING:
+        /*FALLTHRU*/
     case ZT_ACID:
         if (lev->typ == IRONBARS) {
+            if (damgtype == ZT_LIGHTNING && rn2(10))
+                break;
             if ((lev->wall_info & W_NONDIGGABLE) != 0) {
                 if (see_it)
-                    Norep("The %s corrode somewhat but remain intact.",
+                    Norep("The %s %s somewhat but remain intact.",
+                          (damgtype == ZT_ACID) ? "corrode" : "melt",
                           defsyms[S_bars].explanation);
                 /* but nothing actually happens... */
             } else {
