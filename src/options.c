ef09a388b869778a4c3cfd0561b7b3b18468c21d
SHIRAKATA Kentaro <argrath@ub32.org>
2023-11-11 22:55:23 +0900

    remove unnecessary condition on optfn_pickup_types()
    `opts` here is always non-null, otherwise it leads segv at earlier code.

--- src/options.c
+++ src/options.c
@@ -3230,8 +3230,6 @@ optfn_pickup_types(
         return optn_ok;
     }
     if (req == get_val || req == get_cnf_val) {
-        if (!opts)
-            return optn_err;
         oc_to_str(flags.pickup_types, ocl);
         Sprintf(opts, "%s", ocl[0] ? ocl : "all");
         return optn_ok;
