586748244998db0e34bd20b39c488f8aa73ed33f
Michael Meyer <me@entrez.cc>
2023-11-15 12:41:30 -0500

    Remove unneeded furniture vs earthquake hack
    This is no longer needed after ab37888b365daa63b738c76ae6e67a0210899ff5.

--- src/music.c
+++ src/music.c
@@ -326,17 +326,6 @@ do_earthquake(int force)
             case CORR:
             case ROOM: /* Try to make a pit. */
  do_pit:
-                /* maketrap() won't replace furniture with a trap,
-                   so remove the furniture first */
-                if (levl[x][y].typ != CORR) {
-                    if (levl[x][y].typ != DOOR) {
-                        levl[x][y].typ = ROOM;
-                        /* clear blessed fountain, disturbed grave */
-                        levl[x][y].horizontal = 0;
-                    }
-                    /* clear doormask, altarmask, looted throne */
-                    levl[x][y].flags = 0; /* same as 'doormask = D_NODOOR' */
-                }
                 chasm = maketrap(x, y, PIT);
                 if (!chasm)
                     break; /* no pit if portal at that location */
