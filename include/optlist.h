e42706356044a5b52e30fd43fb243c11ce19f2cd
Michael Meyer <me@entrez.cc>
2023-11-17 12:23:49 -0500

    Enable 'dropped_nopick' by default

--- include/optlist.h
+++ include/optlist.h
@@ -261,8 +261,8 @@ static int optfn_##a(int, int, boolean, char *, char *);
     NHOPTC(dogname, Advanced, PL_PSIZ, opt_in, set_gameview,
                 No, Yes, No, No, NoAlias,
                 "name of your starting pet if it is a little dog")
-    NHOPTB(dropped_nopick, Behavior, 0, opt_in, set_in_game,
-           Off, Yes, No, No, NoAlias, &flags.nopick_dropped, Term_False,
+    NHOPTB(dropped_nopick, Behavior, 0, opt_out, set_in_game,
+           On, Yes, No, No, NoAlias, &flags.nopick_dropped, Term_False,
            "don't autopickup dropped items")
     NHOPTC(dungeon, Advanced, MAXDCHARS + 1,opt_in, set_in_config,
                 No, Yes, No, No, NoAlias,
