e0bc8afa5289aa6d44b85ef5202267e37adbe0e4
nhmall <nhmall@nethack.org>
2023-11-23 20:20:15 -0500

    keep comment and the struct it applies to together
    An code insertion earlier today got between them.

--- include/permonst.h
+++ include/permonst.h
@@ -6,18 +6,6 @@
 #ifndef PERMONST_H
 #define PERMONST_H
 
-/*     This structure covers all attack forms.
- *     aatyp is the gross attack type (eg. claw, bite, breath, ...)
- *     adtyp is the damage type (eg. physical, fire, cold, spell, ...)
- *     damn is the number of hit dice of damage from the attack.
- *     damd is the number of sides on each die.
- *
- *     Some attacks can do no points of damage.  Additionally, some can
- *     have special effects *and* do damage as well.  If damn and damd
- *     are set, they may have a special meaning.  For example, if set
- *     for a blinding attack, they determine the amount of time blinded.
- */
-
 enum monnums {
 #define MONS_ENUM
 #include "monsters.h"
@@ -29,6 +17,18 @@ enum monnums {
                 /* mons[SPECIAL_PM] through mons[NUMMONS-1], inclusive, are
                    never generated randomly and cannot be polymorphed into */
 };
+
+/*     This structure covers all attack forms.
+ *     aatyp is the gross attack type (eg. claw, bite, breath, ...)
+ *     adtyp is the damage type (eg. physical, fire, cold, spell, ...)
+ *     damn is the number of hit dice of damage from the attack.
+ *     damd is the number of sides on each die.
+ *
+ *     Some attacks can do no points of damage.  Additionally, some can
+ *     have special effects *and* do damage as well.  If damn and damd
+ *     are set, they may have a special meaning.  For example, if set
+ *     for a blinding attack, they determine the amount of time blinded.
+ */
 
 struct attack {
     uchar aatyp;
