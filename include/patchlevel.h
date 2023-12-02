3c82780c1cf1a28d0f06825a38154a8e2ea88117
nhmall <nhmall@nethack.org>
2023-12-02 11:31:58 -0500

    bump patchlevel for u field addition

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -17,7 +17,7 @@
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
  */
-#define EDITLEVEL 89
+#define EDITLEVEL 90
 
 /*
  * Development status possibilities.
