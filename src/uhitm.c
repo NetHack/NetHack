01d6d94e30f82ceca9c0dd20a88800ba877b4013
nhmall <nhmall@nethack.org>
2023-12-29 00:34:37 -0500

    non-Null handling for uhitm.c

--- src/uhitm.c
+++ src/uhitm.c
@@ -8,56 +8,62 @@
 static const char brief_feeling[] =
     "have a %s feeling for a moment, then it passes.";
 
-static boolean mhitm_mgc_atk_negated(struct monst *, struct monst *, boolean);
+static boolean mhitm_mgc_atk_negated(struct monst *, struct monst *,
+                                     boolean) NONNULLPTRS;
 static boolean known_hitum(struct monst *, struct obj *, int *, int, int,
-                           struct attack *, int);
-static boolean theft_petrifies(struct obj *);
-static void steal_it(struct monst *, struct attack *);
-static boolean hitum_cleave(struct monst *, struct attack *);
+                           struct attack *, int) NONNULLARG13;
+static boolean theft_petrifies(struct obj *) NONNULLARG1;
+static void steal_it(struct monst *, struct attack *) NONNULLARG1;
+/* hitum_cleave() has contradictory information. There's a comment
+ * beside the 1st arg 'target' stating non-null, but later on there
+ * is a test for 'target' being null */
+static boolean hitum_cleave(struct monst *, struct attack *) NO_NNARGS;
 static boolean double_punch(void);
-static boolean hitum(struct monst *, struct attack *);
-static void hmon_hitmon_barehands(struct _hitmon_data *, struct monst *);
+static boolean hitum(struct monst *, struct attack *) NONNULLARG1;
+static void hmon_hitmon_barehands(struct _hitmon_data *,
+		                  struct monst *) NONNULLARG12;
 static void hmon_hitmon_weapon_ranged(struct _hitmon_data *, struct monst *,
-                                      struct obj *);
+                                      struct obj *) NONNULLARG123;
 static void hmon_hitmon_weapon_melee(struct _hitmon_data *, struct monst *,
-                                     struct obj *);
+                                     struct obj *) NONNULLARG123;
 static void hmon_hitmon_weapon(struct _hitmon_data *, struct monst *,
-                               struct obj *);
+                               struct obj *) NONNULLARG123;
 static void hmon_hitmon_potion(struct _hitmon_data *, struct monst *,
-                               struct obj *);
+                               struct obj *) NONNULLARG123;
 static void hmon_hitmon_misc_obj(struct _hitmon_data *, struct monst *,
-                                 struct obj *);
+                                 struct obj *) NONNULLARG123;
 static void hmon_hitmon_do_hit(struct _hitmon_data *, struct monst *,
-                               struct obj *);
+                               struct obj *) NONNULLARG12;
 static void hmon_hitmon_dmg_recalc(struct _hitmon_data *, struct obj *);
 static void hmon_hitmon_poison(struct _hitmon_data *, struct monst *,
-                               struct obj *);
+                               struct obj *) NONNULLARG123;
 static void hmon_hitmon_jousting(struct _hitmon_data *, struct monst *,
-                                 struct obj *);
+                                 struct obj *) NONNULLARG123;
 static void hmon_hitmon_stagger(struct _hitmon_data *, struct monst *,
-                                struct obj *);
+                                struct obj *) NONNULLARG12;
 static void hmon_hitmon_pet(struct _hitmon_data *, struct monst *,
-                            struct obj *);
+                            struct obj *) NONNULLARG12;
 static void hmon_hitmon_splitmon(struct _hitmon_data *, struct monst *,
-                                 struct obj *);
+                                 struct obj *) NONNULLARG12;
 static void hmon_hitmon_msg_hit(struct _hitmon_data *, struct monst *,
-                                struct obj *);
+                                struct obj *) NONNULLARG12;
 static void hmon_hitmon_msg_silver(struct _hitmon_data *, struct monst *,
-                                   struct obj *);
+                                   struct obj *) NONNULLARG12;
 static void hmon_hitmon_msg_lightobj(struct _hitmon_data *, struct monst *,
-                                     struct obj *);
-static boolean hmon_hitmon(struct monst *, struct obj *, int, int);
-static int joust(struct monst *, struct obj *);
+                                     struct obj *) NONNULLARG12;
+static boolean hmon_hitmon(struct monst *, struct obj *, int, int) NONNULLARG1;
+static int joust(struct monst *, struct obj *) NONNULLARG12;
 static void demonpet(void);
-static boolean m_slips_free(struct monst *, struct attack *);
-static void start_engulf(struct monst *);
+static boolean m_slips_free(struct monst *, struct attack *) NONNULLPTRS;
+static void start_engulf(struct monst *) NONNULLARG1;
 static void end_engulf(void);
-static int gulpum(struct monst *, struct attack *);
-static boolean hmonas(struct monst *);
-static void nohandglow(struct monst *);
-static boolean mhurtle_to_doom(struct monst *, int, struct permonst **);
-static void first_weapon_hit(struct obj *);
-static boolean shade_aware(struct obj *);
+static int gulpum(struct monst *, struct attack *) NONNULLPTRS;
+static boolean hmonas(struct monst *) NONNULLARG1;
+static void nohandglow(struct monst *) NONNULLARG1;
+static boolean mhurtle_to_doom(struct monst *, int,
+                               struct permonst **) NONNULLARG13;
+static void first_weapon_hit(struct obj *) NONNULLARG1;
+static boolean shade_aware(struct obj *) NO_NNARGS;
 
 #define PROJECTILE(obj) ((obj) && is_ammo(obj))
 
