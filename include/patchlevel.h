aeab3f69a622ec81c37f0654716ba7e2a2e482cf
nhmall <nhmall@nethack.org>
2024-01-01 11:04:26 -0500

    update year in copyright to 2024

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -36,7 +36,7 @@
 #define DEBUG
 #endif
 
-#define COPYRIGHT_BANNER_A "NetHack, Copyright 1985-2023"
+#define COPYRIGHT_BANNER_A "NetHack, Copyright 1985-2024"
 #define COPYRIGHT_BANNER_B \
     "         By Stichting Mathematisch Centrum and M. Stephenson."
 /* nomakedefs.copyright_banner_c is generated at runtime */
