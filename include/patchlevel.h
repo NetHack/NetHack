02c675078b3397b6476749d7af1577445a6414f0
PatR <rankin@nethack.org>
2024-01-10 23:22:56 -0800

    PR #1150 - rework erinyes into avenging Furies
    Pull request from entrez:  change erinyes from lame devils named
    after the Furies of Greek myth into those Furies.
    
    Bumps EDITLEVEL because of changes to saved data.  I augmented the
    new data.base entry.
    
    Closes #1150

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -17,7 +17,7 @@
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
  */
-#define EDITLEVEL 95
+#define EDITLEVEL 96
 
 /*
  * Development status possibilities.
