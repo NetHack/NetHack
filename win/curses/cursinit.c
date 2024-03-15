464d0c9269bc6c35a942ff94f7a803f3160216a0
Pasi Kallinen <paxed@alt.org>
2024-03-15 11:06:36 +0200

    Curses: swap the grey and no-color initialization
    The grey color was initialized as default foreground color,
    and no-color as white.

--- win/curses/cursinit.c
+++ win/curses/cursinit.c
@@ -310,7 +310,7 @@ curses_init_nhcolors(void)
         init_pair(5, COLOR_BLUE, -1);
         init_pair(6, COLOR_MAGENTA, -1);
         init_pair(7, COLOR_CYAN, -1);
-        init_pair(8, -1, -1);
+        init_pair(8, COLOR_WHITE, -1);
 
         {
             int i;
@@ -351,7 +351,7 @@ curses_init_nhcolors(void)
                 init_pair(1, COLOR_BLACK + 8, -1);
             }
 # endif
-            init_pair(9, COLOR_WHITE, -1);
+            init_pair(9, -1, -1);
             init_pair(10, COLOR_RED + 8, -1);
             init_pair(11, COLOR_GREEN + 8, -1);
             init_pair(12, COLOR_YELLOW + 8, -1);
