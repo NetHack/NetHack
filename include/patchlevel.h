b844632dfd93a211ab167a209798119e85a0f423
Patric Mueller <bhaak@gmx.net>
2024-07-11 16:35:24 +0200

    bump EDITLEVEL for 0447a1f10
    Commit 0447a1f10 accidentally broke save compatibility.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -17,7 +17,7 @@
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
  */
-#define EDITLEVEL 103
+#define EDITLEVEL 104
 
 /*
  * Development status possibilities.
