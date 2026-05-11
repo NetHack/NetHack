e25e8362b17857bd059a607072bb39ba6960a41b
Ingo Paschke <ipaschke@lpclabs.de>
2026-05-11 16:46:28 +0200

    Amiga: clean up amiconf.h
    Remove legacy compiler guards and stale extern declarations (the
    ami_wbench_* family, CopyFile, ami_argset, ami_mkargline, FromWBench).
    Drop unused AMII_*_VOLUME / DEFAULT_ICON macros and the
    IDCMP_CLOSEWINDOW auto-define.

--- include/amiconf.h
+++ include/amiconf.h
@@ -7,41 +7,17 @@
 #define AMICONF_H
 
 #undef abs /* avoid using macro form of abs */
-#ifndef __SASC_60
 #undef min /* this gets redefined */
 #undef max /* this gets redefined */
-#endif
 
 #include <time.h> /* get time_t defined before use! */
 
-#ifdef CROSS_TO_AMIGA
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
 #include <dos/dos.h>
 #include <clib/dos_protos.h>
 #include <proto/dos.h>
-#endif
-
-#ifdef __SASC_60    /* since SAS can prevent re-inclusion */
-#include <stdlib.h> /* general things, including builtins */
-#include <string.h>
-#endif
-
-#ifdef AZTEC_50
-#include <stdlib.h>
-#define AZTEC_C_WORKAROUND /* Bug which turns up in sounds.c. Bummer... */
-#define NO_SIGNAL          /* 5.0 signal handling doesn't like SIGINT... */
-#endif
-
-#ifdef _DCC
-#include <stdlib.h>
-#define _SIZE_T
-#endif
-
-#ifndef __GNUC__
-typedef long off_t;
-#endif
 
 #define MICRO /* must be defined to allow some inclusions */
 
@@ -56,65 +32,29 @@ typedef long off_t;
 /* nhsdat sound library not used in 5.0 */
 #undef DLBFILE2
 
-#ifndef CROSS_TO_AMIGA
-#define FILENAME_CMP stricmp /* case insensitive */
-#else
 #define FILENAME_CMP strcmpi /* case insensitive */
-#endif
-
-#ifndef __SASC_60
 #define O_BINARY 0
-#endif
-
-/* Compile in New Intuition look for 2.0 */
-#ifdef IDCMP_CLOSEWINDOW
-#ifndef INTUI_NEW_LOOK
-#define INTUI_NEW_LOOK 1
-#endif
-#endif
 
 #define MFLOPPY /* You'll probably want this; provides assistance \
                  * for typical personal computer configurations   \
                  */
-#ifndef CROSS_TO_AMIGA
-#define RANDOM
-#endif
 
 /* ### amidos.c ### */
 
 extern void nethack_exit(int);
 
-/* ### amiwbench.c ### */
-
-extern void ami_wbench_init(void);
-extern void ami_wbench_args(void);
-extern int ami_wbench_getsave(int);
-extern void ami_wbench_unlink(char *);
-extern int ami_wbench_iconsize(char *);
-extern void ami_wbench_iconwrite(char *);
-extern int ami_wbench_badopt(const char *);
-extern void ami_wbench_cleanup(void);
-extern void getlind(const char *, char *, const char *);
-
 /* ### winreq.c ### */
 
 extern void amii_setpens(int);
 
+extern void getlind(const char *, char *, const char *);
 extern void exit(int);
 extern void CleanUp(void);
 extern void Abort(long);
 extern int getpid(void);
-extern char *CopyFile(const char *, const char *);
 extern int kbhit(void);
 extern int WindowGetchar(void);
-extern void ami_argset(int *, char *[]);
-extern void ami_mkargline(int *, char **[]);
 extern void ami_wininit_data(int);
-
-#define FromWBench 0 /* A hint for compiler ... */
-/* extern boolean FromWBench;	/* how were we run? */
-extern int ami_argc;
-extern char **ami_argv;
 
 #ifndef MICRO_H
 #include "micro.h"
@@ -125,34 +65,14 @@ extern char **ami_argv;
 #endif
 
 #define remove(x) unlink(x)
-
-/* DICE wants rewind() to return void.	We want it to return int. */
-#if defined(_DCC) || defined(__GNUC__)
 #define rewind(f) fseek(f, 0, 0)
-#endif
-
-#ifdef AZTEC_C
-extern FILE *freopen(const char *, const char *, FILE *);
-extern char *gets(char *);
-#endif
-
-/*
- * If AZTEC_C  we can't use the long cpath in vision.c....
- */
-#ifdef AZTEC_C
-#undef MACRO_CPATH
-#endif
 
 /*
  *  (Possibly) configurable Amiga options:
  */
 
 #define HACKFONT  /* Use special hack.font */
-#ifndef CROSS_TO_AMIGA   /* issues with prototype and spawnl */
-#define SHELL  /* Have a shell escape command (!) */
-#endif
 #define MAIL      /* Get mail at unexpected occasions */
-#define DEFAULT_ICON "NetHack:default.icon" /* private icon */
 #define AMIFLUSH /* toss typeahead (select flush in .cnf) */
 
 /* new window system options */
@@ -170,32 +90,18 @@ typedef unsigned short AMII_COLOR_TYPE;
 
 #undef TERMLIB
 
-#define AMII_MUFFLED_VOLUME 40
-#define AMII_SOFT_VOLUME 50
-#define AMII_OKAY_VOLUME 60
-#define AMII_LOUDER_VOLUME 80
-
-#ifdef TTY_GRAPHICS
-#define ANSI_DEFAULT
-#endif
-
 #ifdef AMII_GRAPHICS
 extern int amii_numcolors;
 void amii_setpens(int);
 #endif
 
-/* for cmd.c: override version in micro.h */
-#ifdef __SASC_60
-#undef M
-#define M(c) ((c) -128)
-#endif
 struct ami_sysflags {
     char sysflagsid[10];
 #ifdef AMIFLUSH
     boolean altmeta;  /* use ALT keys as META */
     boolean amiflush; /* kill typeahead */
 #endif
-#ifdef AMII_GRAPHICS 
+#ifdef AMII_GRAPHICS
     int numcols;
     unsigned short amii_dripens[20]; /* DrawInfo Pens currently there are 13 in v39 */
     AMII_COLOR_TYPE amii_curmap[AMII_MAXCOLORS]; /* colormap */
