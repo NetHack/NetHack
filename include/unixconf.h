35d1cac7e85714529757c5f1cb6adefef6cc8a6d
Anthony Howe <achowe@snert.com>
2026-05-15 10:26:40 -0400

    Unoffical GhostBSD / FreeBSD configuration and compile fixes.

--- include/unixconf.h
+++ include/unixconf.h
@@ -338,7 +338,7 @@
 #endif
 
 #if defined(BSD) || defined(ULTRIX)
-#if !defined(DGUX) && !defined(SUNOS4) && !defined(__NetBSD__)
+#if !defined(DGUX) && !defined(SUNOS4) && !defined(__NetBSD__) && !defined(__FreeBSD__)
 #define memcpy(d, s, n) bcopy(s, d, n)
 #define memcmp(s1, s2, n) bcmp(s2, s1, n)
 #endif
