c82d61561c634255c4d23611173d81fb5a7c167b
Ingo Paschke <ipaschke@lpclabs.de>
2026-06-12 11:13:00 +0200

    atari: share one keypad translation table

--- sys/atari/tos.c
+++ sys/atari/tos.c
@@ -54,58 +54,7 @@ tgetch(void)
 /*
  *  Keyboard translation tables.
  */
-#define KEYPADLO 0x61
-#define KEYPADHI 0x71
-
-#define PADKEYS (KEYPADHI - KEYPADLO + 1)
-#define iskeypad(x) (KEYPADLO <= (x) && (x) <= KEYPADHI)
-
-/*
- * Keypad keys are translated to the normal values below.
- * When iflags.BIOS is active, shifted keypad keys are translated to the
- *    shift values below.
- */
-static const struct pad {
-    char normal, shift, cntrl;
-} keypad[PADKEYS] =
-    {
-      { C('['), 'Q', C('[') }, /* UNDO */
-      { '?', '/', '?' },       /* HELP */
-      { '(', 'a', '(' },       /* ( */
-      { ')', 'w', ')' },       /* ) */
-      { '/', '/', '/' },       /* / */
-      { C('p'), '$', C('p') }, /* * */
-      { 'y', 'Y', C('y') },    /* 7 */
-      { 'k', 'K', C('k') },    /* 8 */
-      { 'u', 'U', C('u') },    /* 9 */
-      { 'h', 'H', C('h') },    /* 4 */
-      { '.', '.', '.' },
-      { 'l', 'L', C('l') }, /* 6 */
-      { 'b', 'B', C('b') }, /* 1 */
-      { 'j', 'J', C('j') }, /* 2 */
-      { 'n', 'N', C('n') }, /* 3 */
-      { 'i', 'I', C('i') }, /* Ins */
-      { '.', ':', ':' }     /* Del */
-    },
-  numpad[PADKEYS] = {
-      { C('['), 'Q', C('[') }, /* UNDO */
-      { '?', '/', '?' },       /* HELP */
-      { '(', 'a', '(' },       /* ( */
-      { ')', 'w', ')' },       /* ) */
-      { '/', '/', '/' },       /* / */
-      { C('p'), '$', C('p') }, /* * */
-      { '7', M('7'), '7' },    /* 7 */
-      { '8', M('8'), '8' },    /* 8 */
-      { '9', M('9'), '9' },    /* 9 */
-      { '4', M('4'), '4' },    /* 4 */
-      { '.', '.', '.' },       /* 5 */
-      { '6', M('6'), '6' },    /* 6 */
-      { '1', M('1'), '1' },    /* 1 */
-      { '2', M('2'), '2' },    /* 2 */
-      { '3', M('3'), '3' },    /* 3 */
-      { 'i', 'I', C('i') },    /* Ins */
-      { '.', ':', ':' }        /* Del */
-  };
+#include "keytrans.h"
 
 /*
  * Unlike Ctrl-letter, the Alt-letter keystrokes have no specific ASCII
